70247cbc813028a561bf13cdecfa59778505c492
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-02-04 02:59:48 +0000

    Refactored private member variables to use interface types rather than concrete types Added use of BlogCategory Streamlined service() method Fixed potential bug with updating blog categories if a blog category was removed and then added again

--- blojsom/src/org/ignition/blojsom/servlet/BlojsomServlet.java
+++ blojsom/src/org/ignition/blojsom/servlet/BlojsomServlet.java
@@ -33,6 +33,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ignition.blojsom.blog.Blog;
 import org.ignition.blojsom.blog.BlogEntry;
+import org.ignition.blojsom.blog.BlogCategory;
 import org.ignition.blojsom.dispatcher.GenericDispatcher;
 import org.ignition.blojsom.util.BlojsomConstants;
 import org.ignition.blojsom.util.BlojsomUtils;
@@ -52,7 +53,7 @@ import java.util.*;
  *
  * @author David Czarnecki
  */
-public class BlojsomServlet extends HttpServlet {
+public class BlojsomServlet extends HttpServlet implements BlojsomConstants {
 
     private final static String BLOG_HOME_IP = "blog-home";
     private final static String BLOG_NAME_IP = "blog-name";
@@ -65,8 +66,6 @@ public class BlojsomServlet extends HttpServlet {
     private final static String BLOG_FLAVOR_CONFIGURATION_IP = "blog-flavor-configuration";
     private final static String BLOG_TEMPLATE_MAP_PROPERTIES_IP = "template-map-properties";
 
-    private final static String FLAVOR_PARAM = "flavor";
-
     private String _blogHome;
     private String _blogLanguage;
     private String _blogDescription;
@@ -74,14 +73,15 @@ public class BlojsomServlet extends HttpServlet {
     private int _blogDepth;
     private String _blogURL;
     private String[] _blogFileExtensions;
-    private TreeMap _blogEntryMap;
     private Blog _blog;
     private long _blogReloadCheck;
 
-    private HashMap _flavorToTemplateMap;
-    private HashMap _flavorToContentTypeMap;
-    private HashMap _flavors;
-    private HashMap _templateDispatchers;
+    private Map _blogEntryMap;
+
+    private Map _flavorToTemplateMap;
+    private Map _flavorToContentTypeMap;
+    private Map _flavors;
+    private Map _templateDispatchers;
 
     private Thread _blogUpdaterThread;
 
@@ -219,7 +219,7 @@ public class BlojsomServlet extends HttpServlet {
     private void recursiveBlogBuilder(int blogDepth, String blogDirectory) {
         blogDepth++;
         _logger.debug("Working in directory: " + blogDirectory + " at blog depth: " + blogDepth);
-        if (_blogDepth != BlojsomConstants.INFINITE_BLOG_DEPTH) {
+        if (_blogDepth != INFINITE_BLOG_DEPTH) {
             if (blogDepth == _blogDepth) {
                 _logger.debug("Reached maximum blog depth: " + blogDepth);
                 return;
@@ -233,18 +233,20 @@ public class BlojsomServlet extends HttpServlet {
         if (!categoryKey.endsWith("/")) {
             categoryKey += "/";
         }
+        BlogCategory blogCategory = new BlogCategory(categoryKey, _blogURL + BlojsomUtils.removeInitialSlash(categoryKey));
 
         // look for blog entries in this directory
         File[] entries = blog.listFiles(BlojsomUtils.getExtensionsFilter(_blogFileExtensions));
         if (entries == null) {
             _logger.debug("No blog entries in blog directory: " + blogDirectory);
-            _blogEntryMap.put(categoryKey, null);
+            _blogEntryMap.put(blogCategory, null);
         } else {
-            TreeMap entryMap;
-            if (_blogEntryMap.get(categoryKey) == null) {
+            Map entryMap;
+            if (!_blogEntryMap.containsKey(blogCategory)) {
                 entryMap = new TreeMap(BlojsomUtils.FILE_TIME_COMPARATOR);
             } else {
-                entryMap = (TreeMap) _blogEntryMap.get(categoryKey);
+                entryMap = (Map) _blogEntryMap.get(blogCategory);
+                _blogEntryMap.remove(blogCategory);
             }
             _logger.debug("Adding " + entries.length + " entries to the blog");
             for (int i = 0; i < entries.length; i++) {
@@ -272,8 +274,9 @@ public class BlojsomServlet extends HttpServlet {
                     }
                 }
             }
+            blogCategory.setNumberOfEntries(entryMap.size());
             _logger.debug("Added " + entryMap.size() + " entries to the blog");
-            _blogEntryMap.put(categoryKey, entryMap);
+            _blogEntryMap.put(blogCategory, entryMap);
         }
 
         if (directories == null) {
@@ -293,16 +296,78 @@ public class BlojsomServlet extends HttpServlet {
         Iterator categoryIterator = _blogEntryMap.keySet().iterator();
         ArrayList deletedCategories = new ArrayList();
         while (categoryIterator.hasNext()) {
-            String category = (String) categoryIterator.next();
-            File blogDirectory = new File(_blogHome + category);
+            BlogCategory blogCategory = (BlogCategory) categoryIterator.next();
+            File blogDirectory = new File(_blogHome + blogCategory.getCategory());
             if (!blogDirectory.exists()) {
                 _logger.debug("Blog directory deleted: " + blogDirectory.toString());
-                deletedCategories.add(category);
+                deletedCategories.add(blogCategory);
             }
         }
         for (int i = 0; i < deletedCategories.size(); i++) {
             _blogEntryMap.remove(deletedCategories.get(i));
         }
+    }
+
+    /**
+     * Retrieve a permalink entry from the entries for a given category
+     *
+     * @param entriesForCategory Blog entries for a given category
+     * @param requestedCategory Requested category
+     * @param permalink Permalink entry requested
+     * @return Blog entry array containing the single requested permalink entry (possibly null), or null if the permalink entry was not found
+     */
+    private BlogEntry[] getPermalinkEntry(Map entriesForCategory, String requestedCategory, String permalink) {
+        BlogEntry[] entryArray;
+
+        boolean foundEntry = false;
+        entryArray = new BlogEntry[1];
+        _logger.debug("Permalink entry: " + requestedCategory + permalink);
+        Iterator entryIterator = entriesForCategory.keySet().iterator();
+        while (entryIterator.hasNext() && !foundEntry) {
+            File entryKey = (File) entryIterator.next();
+            if (entryKey.getName().endsWith(permalink)) {
+                foundEntry = true;
+                entryArray[0] = (BlogEntry) entriesForCategory.get(entryKey);
+                // Check if entry deleted from disk
+                if (entryArray[0] == null) {
+                    entryArray = null;
+                    entriesForCategory.remove(entryKey);
+                }
+            }
+        }
+
+        if (foundEntry == false) {
+            _logger.warn("Permalink entry: " + permalink + " gone");
+            entryArray = null;
+        }
+
+        return entryArray;
+    }
+
+    /**
+     * Retrieve all of the entries for a requested category
+     *
+     * @param entriesForCategory Entries for the requested category
+     * @param requestedCategory Requested category
+     * @return Entry array containing the list of blog entries for the requested category
+     */
+    private BlogEntry[] getEntriesForCategory(Map entriesForCategory) {
+        BlogEntry[] entryArray;
+        ArrayList entryList = new ArrayList();
+        entryArray = new BlogEntry[entriesForCategory.size()];
+        Iterator entryIterator = entriesForCategory.keySet().iterator();
+        while (entryIterator.hasNext()) {
+            Object entryKey = entryIterator.next();
+            if (entriesForCategory.get(entryKey) != null) {
+                entryList.add(entriesForCategory.get(entryKey));
+            } else {
+                // Entry deleted from disk
+                entriesForCategory.remove(entryKey);
+            }
+        }
+        entryArray = (BlogEntry[]) entryList.toArray(new BlogEntry[entryList.size()]);
+
+        return entryArray;
     }
 
     /**
@@ -316,10 +381,10 @@ public class BlojsomServlet extends HttpServlet {
     public void service(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String blogSiteURL = BlojsomUtils.getBlogSiteURL(_blogURL, httpServletRequest.getServletPath());
 
-        // Determine user requested category
+        // Determine the user requested category
         String requestedCategory = httpServletRequest.getPathInfo();
 
-        // Determine permalink
+        // Determine if a permalink has been requested
         String permalink = httpServletRequest.getParameter("permalink");
         if (permalink != null) {
             _logger.debug("Permalink request for: " + permalink);
@@ -330,79 +395,55 @@ public class BlojsomServlet extends HttpServlet {
         } else if (!requestedCategory.endsWith("/")) {
             requestedCategory += "/";
         }
+
         _logger.debug("User requested category: " + requestedCategory);
+        BlogCategory category = new BlogCategory(requestedCategory, _blogURL + BlojsomUtils.removeInitialSlash(requestedCategory));
 
         String flavor = httpServletRequest.getParameter(FLAVOR_PARAM);
         if (flavor == null) {
-            flavor = BlojsomConstants.DEFAULT_FLAVOR_HTML;
+            flavor = DEFAULT_FLAVOR_HTML;
         } else {
             if (_flavors.get(flavor) == null) {
-                flavor = BlojsomConstants.DEFAULT_FLAVOR_HTML;
+                flavor = DEFAULT_FLAVOR_HTML;
             }
         }
 
-        TreeMap entriesForCategory = (TreeMap) _blogEntryMap.get(requestedCategory);
+        // Get the entries for the requested category
+        Map entriesForCategory = (Map) _blogEntryMap.get(category);
         if (entriesForCategory == null) {
-            entriesForCategory = (TreeMap) _blogEntryMap.get("/");
+            entriesForCategory = (Map) _blogEntryMap.get(new BlogCategory("/", _blogURL));
         }
 
-        HashMap context = new HashMap();
-        context.put(BlojsomConstants.BLOJSOM_BLOG, _blog);
-        BlogEntry[] entryArray;
-        ArrayList entryList = new ArrayList();
-        if (permalink == null) {
-            entryArray = new BlogEntry[entriesForCategory.size()];
-            Iterator entryIterator = entriesForCategory.keySet().iterator();
-            while (entryIterator.hasNext()) {
-                Object entryKey = entryIterator.next();
-                if (entriesForCategory.get(entryKey) != null) {
-                    entryList.add(entriesForCategory.get(entryKey));
-                } else {
-                    // Entry deleted from disk
-                    entriesForCategory.remove(entryKey);
-                }
-            }
-            entryArray = (BlogEntry[]) entryList.toArray(new BlogEntry[entryList.size()]);
+        // Convert the entries from the map into an array
+        BlogEntry[] entries;
+        if (permalink != null) {
+            entries = getPermalinkEntry(entriesForCategory, requestedCategory, permalink);
         } else {
-            boolean foundEntry = false;
-            entryArray = new BlogEntry[1];
-            _logger.debug("Permalink entry: " + requestedCategory + permalink);
-            Iterator entryIterator = entriesForCategory.keySet().iterator();
-            while (entryIterator.hasNext() && !foundEntry) {
-                File entryKey = (File) entryIterator.next();
-                if (entryKey.getName().endsWith(permalink)) {
-                    foundEntry = true;
-                    entryArray[0] = (BlogEntry) entriesForCategory.get(entryKey);
-                    // Check if entry deleted from disk
-                    if (entryArray[0] == null) {
-                        entryArray = null;
-                        entriesForCategory.remove(entryKey);
-                    }
-                }
-            }
-            if (foundEntry == false) {
-                _logger.warn("Permalink entry: " + permalink + " gone");
-                entryArray = null;
-            }
+            entries = getEntriesForCategory(entriesForCategory);
         }
 
-        context.put(BlojsomConstants.BLOJSOM_SITE_URL, blogSiteURL);
-        context.put(BlojsomConstants.BLOJSOM_ENTRIES, entryArray);
-        context.put(BlojsomConstants.BLOJSOM_CATEGORIES, BlojsomUtils.getBlogCategories(_blogURL, _blogEntryMap));
+        // Setup the context for the dispatcher
+        HashMap context = new HashMap();
+        context.put(BLOJSOM_BLOG, _blog);
+        context.put(BLOJSOM_SITE_URL, blogSiteURL);
+        context.put(BLOJSOM_ENTRIES, entries);
+        context.put(BLOJSOM_CATEGORIES, BlojsomUtils.getBlogCategories(_blogURL, _blogEntryMap));
 
         // Forward the request on to the template for the requested flavor
         String flavorTemplate;
         if (_flavorToTemplateMap.get(flavor) == null) {
-            flavorTemplate = (String) _flavorToTemplateMap.get(BlojsomConstants.DEFAULT_FLAVOR_HTML);
+            flavorTemplate = (String) _flavorToTemplateMap.get(DEFAULT_FLAVOR_HTML);
         } else {
             flavorTemplate = (String) _flavorToTemplateMap.get(flavor);
         }
 
+        // Get the content type for the requested flavor
         String flavorContentType = (String) _flavorToContentTypeMap.get(flavor);
 
         String templateExtension = BlojsomUtils.getFileExtension(flavorTemplate);
         _logger.debug("Template extension: " + templateExtension);
 
+        // Retrieve the appropriate dispatcher for the template
         GenericDispatcher dispatcher = (GenericDispatcher) _templateDispatchers.get(templateExtension);
         dispatcher.dispatch(httpServletRequest, httpServletResponse, context, flavorTemplate, flavorContentType);
     }
