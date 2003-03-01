6cb60fd5e98507d5249802e6418db927290ae98f
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-03-01 18:03:30 +0000

    Added license header and moved QUERY_PARAM to this file

--- blojsom/plugins/src/org/ignition/blojsom/plugin/SimpleSearchPlugin.java
+++ blojsom/plugins/src/org/ignition/blojsom/plugin/SimpleSearchPlugin.java
@@ -1,21 +1,56 @@
+/**
+ * Copyright (c) 2003, David A. Czarnecki
+ * All rights reserved.
+ *
+ * Portions Copyright (c) 2003 by Mark Lussier
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the "David A. Czarnecki" nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.ignition.blojsom.plugin;
 
-import org.ignition.blojsom.blog.BlogEntry;
-import org.ignition.blojsom.util.BlojsomConstants;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.ignition.blojsom.blog.BlogEntry;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * SimpleSearchPlugin
  *
  * @author David Czarnecki
  */
-public class SimpleSearchPlugin implements BlojsomPlugin, BlojsomConstants {
+public class SimpleSearchPlugin implements BlojsomPlugin {
+
+    /**
+     * Request parameter for the "query"
+     */
+    private static final String QUERY_PARAM = "query";
 
     private Log _logger = LogFactory.getLog(SimpleSearchPlugin.class);
 
@@ -47,6 +82,8 @@ public class SimpleSearchPlugin implements BlojsomPlugin, BlojsomConstants {
         String query = httpServletRequest.getParameter(QUERY_PARAM);
         if (query == null) {
             return entries;
+        } else {
+            _logger.debug("User query: " + query);
         }
 
         ArrayList entriesMatchingQuery = new ArrayList(5);
