1858367945d74bf9be4605dce51de5981e3ff883
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-03-01 18:02:23 +0000

    Added constructor for taking just a Throwable

--- blojsom/src/org/ignition/blojsom/plugin/BlojsomPluginException.java
+++ blojsom/src/org/ignition/blojsom/plugin/BlojsomPluginException.java
@@ -33,7 +33,7 @@ package org.ignition.blojsom.plugin;
  * BlojsomPluginException
  *
  * @author David Czarnecki
- * @version $Id: BlojsomPluginException.java,v 1.1 2003-02-25 19:06:11 czarneckid Exp $
+ * @version $Id: BlojsomPluginException.java,v 1.2 2003-03-01 18:02:23 czarneckid Exp $
  */
 public class BlojsomPluginException extends Exception {
 
@@ -60,5 +60,22 @@ public class BlojsomPluginException extends Exception {
      */
     public BlojsomPluginException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and a detail
+     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+     * typically contains the class and detail message of <tt>cause</tt>).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * java.security.PrivilegedActionException}).
+     *
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
+    public BlojsomPluginException(Throwable cause) {
+        super(cause);
     }
 }
