b07c6f8405b8506cf7275a91dfdb0435c15e4148
intabulas <intabulas@28e03f91-8513-0410-9821-a6140ff45be5>
2003-03-13 00:04:42 +0000

    Added Referer Plugin

---
+++ blojsom/plugins/src/org/ignition/blojsom/plugin/RefererLogPlugin.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2003, David A. Czarnecki
+ * All rights reserved.
+ *
+ * Portions Copyright (c) 2003 by Mark Lussier
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the "David A. Czarnecki" nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.ignition.blojsom.plugin;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ignition.blojsom.blog.BlogEntry;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Enumeration;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Generic Referer Plugin
+ * This plugin will log to a file all the http referes it see's. It makes no attempt to filter, sort and resolve
+ * duplicates (that is an excercise for the reader). It dumps this log to what ever file you set in the refer-log
+ * init-param in web.xml. If not file is setup, it will dump it to the log as a backup
+ *
+ * @author Mark Lussier
+ * @version $Id: RefererLogPlugin.java,v 1.1 2003-03-12 23:59:43 intabulas Exp $
+ */
+public class RefererLogPlugin implements BlojsomPlugin {
+
+    /**
+     * HTTP Header for Referer Information
+     */
+    private static final String HEADER_REFERER = "referer";
+
+    private String _refererlog = null;
+
+    private Log _logger = LogFactory.getLog(RefererLogPlugin.class);
+
+    /**
+     * Initialize this plugin. This method only called when the plugin is instantiated.
+     *
+     * @param servletConfig Servlet config object for the plugin to retrieve any initialization parameters
+     * @param blogProperties Read-only properties for the Blog
+     * @throws BlojsomPluginException If there is an error initializing the plugin
+     */
+    public void init(ServletConfig servletConfig, HashMap blogProperties) throws BlojsomPluginException {
+        _refererlog = servletConfig.getInitParameter("referer-log");
+    }
+
+    /**
+     * Process the blog entries
+     *
+     * @param httpServletRequest Request
+     * @param entries Blog entries retrieved for the particular request
+     * @return Modified set of blog entries
+     * @throws BlojsomPluginException If there is an error processing the blog entries
+     */
+    public BlogEntry[] process(HttpServletRequest httpServletRequest, BlogEntry[] entries) throws BlojsomPluginException {
+
+        String _referer = httpServletRequest.getHeader(HEADER_REFERER) ;
+
+        if ( _refererlog != null ) {
+            try {
+                String output = _referer + "\n";
+                FileOutputStream _fos = new FileOutputStream(_refererlog, true);
+                _fos.write( output.getBytes());
+                _fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            _logger.info("HTTP Referer is " + _referer);
+        }
+
+        return entries;
+    }
+
+    /**
+     * Perform any cleanup for the plugin. Called after {@link #process}.
+     *
+     * @throws BlojsomPluginException If there is an error performing cleanup for this plugin
+     */
+    public void cleanup() throws BlojsomPluginException {
+    }
+
+
+
+}
