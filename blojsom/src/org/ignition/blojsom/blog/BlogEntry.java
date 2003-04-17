8bc657e6db288ec2618d271233cd2d0d2fb5a8a7
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-04-17 02:34:45 +0000

    Moved all file-backed operations of BlogEntry into subclass, FileBackedBlogEntry.

--- blojsom/src/org/ignition/blojsom/blog/BlogEntry.java
+++ blojsom/src/org/ignition/blojsom/blog/BlogEntry.java
@@ -36,59 +36,35 @@ package org.ignition.blojsom.blog;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.ignition.blojsom.util.BlojsomUtils;
 import org.ignition.blojsom.util.BlojsomConstants;
+import org.ignition.blojsom.util.BlojsomUtils;
 
-import java.io.*;
-import java.util.Date;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 
 /**
  * BlogEntry
  *
  * @author David Czarnecki
- * @version $Id: BlogEntry.java,v 1.27 2003-04-13 18:02:57 czarneckid Exp $
+ * @version $Id: BlogEntry.java,v 1.28 2003-04-17 02:34:45 czarneckid Exp $
  */
-public class BlogEntry implements BlojsomConstants {
+public abstract class BlogEntry implements BlojsomConstants {
 
-    private Log _logger = LogFactory.getLog(BlogEntry.class);
+    protected Log _logger = LogFactory.getLog(BlogEntry.class);
 
-    private String _title;
-    private String _link;
-    private String _description;
-    private File _source;
-    private String _category;
-    private Date _entryDate;
-    private long _lastModified;
-    private String _commentsDirectory;
-    private ArrayList _comments;
-    private ArrayList _trackbacks;
-    private String _trackbacksDirectory;
+    protected String _title;
+    protected String _link;
+    protected String _description;
+    protected String _category;
+    protected Date _entryDate;
+    protected long _lastModified;
+    protected ArrayList _comments;
+    protected ArrayList _trackbacks;
 
     /**
      * Create a new blog entry with no data
      */
     public BlogEntry() {
-        _commentsDirectory = DEFAULT_COMMENTS_DIRECTORY;
-        _trackbacksDirectory = DEFAULT_TRACKBACK_DIRECTORY;
-    }
-
-    /**
-     * Create a new blog entry
-     *
-     * @param title Entry title
-     * @param link Permalink to the entry
-     * @param description Entry description
-     * @param source File containing the blog entry
-     */
-    public BlogEntry(String title, String link, String description, File source) {
-        super();
-        _title = title;
-        _link = link;
-        _description = description;
-        _source = source;
-        _entryDate = new Date(_source.lastModified());
     }
 
     /**
@@ -100,6 +76,15 @@ public class BlogEntry implements BlojsomConstants {
      */
     public Date getDate() {
         return _entryDate;
+    }
+
+    /**
+     * Date of this blog entry
+     *
+     * @param entryDate Date of the blog entry
+     */
+    public void setDate(Date entryDate) {
+        _entryDate = entryDate;
     }
 
     /**
@@ -176,24 +161,6 @@ public class BlogEntry implements BlojsomConstants {
     }
 
     /**
-     * File containing the blog entry
-     *
-     * @return Blog entry file
-     */
-    public File getSource() {
-        return _source;
-    }
-
-    /**
-     * Set the file of the blog entry
-     *
-     * @param source File for the blog entry
-     */
-    public void setSource(File source) {
-        _source = source;
-    }
-
-    /**
      * Description of the blog entry
      *
      * @return Blog entry description
@@ -235,49 +202,14 @@ public class BlogEntry implements BlojsomConstants {
      *
      * @return Blog Id
      */
-    public String getId() {
-        return _category + "?" + PERMALINK_PARAM + "=" + BlojsomUtils.urlEncode(_source.getName());
-    }
+    public abstract String getId();
 
     /**
      * Return the permalink name for this blog entry
      *
      * @return Permalink name
      */
-    public String getPermalink() {
-        return BlojsomUtils.urlEncode(_source.getName());
-    }
-
-    /**
-     * Reload the blog entry from disk
-     *
-     * The first line of the blog entry will be used as the title of the blog
-     */
-    public void reloadSource() throws IOException {
-        boolean hasLoadedTitle = false;
-        String lineSeparator = System.getProperty("line.separator");
-
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(_source), UTF8));
-            String line;
-            StringBuffer description = new StringBuffer();
-            while ((line = br.readLine()) != null) {
-                if (!hasLoadedTitle) {
-                    _title = line;
-                    hasLoadedTitle = true;
-                } else {
-                    description.append(line);
-                    description.append(lineSeparator);
-                }
-            }
-            br.close();
-            _description = description.toString();
-            _entryDate = new Date(_source.lastModified());
-            _lastModified = _source.lastModified();
-        } catch (IOException e) {
-            _logger.error(e);
-        }
-    }
+    public abstract String getPermalink();
 
     /**
      * Category for the blog entry
@@ -315,14 +247,11 @@ public class BlogEntry implements BlojsomConstants {
     }
 
     /**
-     * Determines whether or not this blog entry supports comments by testing to see if
-     * the blog entry is writable.
+     * Determines whether or not this blog entry supports comments.
      *
-     * @return <code>true</code> if the blog entry is writable, <code>false</code> otherwise
+     * @return <code>true</code> if the blog entry supports comments, <code>false</code> otherwise
      */
-    public boolean supportsComments() {
-        return _source.canWrite();
-    }
+    public abstract boolean supportsComments();
 
     /**
      * Get the comments
@@ -370,96 +299,6 @@ public class BlogEntry implements BlojsomConstants {
     }
 
     /**
-     * Set the directory for comments
-     *
-     * @param commentsDirectory Comments directory
-     */
-    public void setCommentsDirectory(String commentsDirectory) {
-        _commentsDirectory = commentsDirectory;
-    }
-
-    /**
-     * Convenience method to load the comments for this blog entry. A blog entry can have
-     */
-    public void loadComments() {
-        if (supportsComments()) {
-            String commentsDirectoryPath;
-            if (_source.getParent() == null) {
-                commentsDirectoryPath = File.separator + _commentsDirectory + File.separator + _source.getName();
-            } else {
-                commentsDirectoryPath = _source.getParent() + File.separator + _commentsDirectory + File.separator + _source.getName();
-            }
-            File commentsDirectory = new File(commentsDirectoryPath);
-            File[] comments = commentsDirectory.listFiles(BlojsomUtils.getExtensionFilter(COMMENT_EXTENSION));
-            if ((comments != null) && (comments.length > 0)) {
-                _logger.debug("Adding " + comments.length + " comments to blog entry: " + getPermalink());
-                Arrays.sort(comments, BlojsomUtils.FILE_TIME_ASCENDING_COMPARATOR);
-                _comments = new ArrayList(comments.length);
-                for (int i = 0; i < comments.length; i++) {
-                    File comment = comments[i];
-                    _comments.add(loadComment(comment));
-                }
-            }
-        } else {
-            _logger.debug("Blog entry does not support comments");
-        }
-    }
-
-    /**
-     * Load a comment for this blog entry from disk
-     * Comments must always have the form:<br />
-     * author<br>
-     * author e-mail address<br />
-     * author url<br />
-     * everything else after is the comment
-     *
-     * @param commentFile Comment file
-     * @return BlogComment Blog comment loaded from disk
-     */
-    private BlogComment loadComment(File commentFile) {
-        int commentSwitch = 0;
-        BlogComment comment = new BlogComment();
-        comment.setCommentDateLong(commentFile.lastModified());
-        StringBuffer commentDescription = new StringBuffer();
-        String commentLine;
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(commentFile), UTF8));
-            while ((commentLine = br.readLine()) != null) {
-                switch (commentSwitch) {
-                    case 0:
-                        {
-                            comment.setAuthor(commentLine);
-                            commentSwitch++;
-                            break;
-                        }
-                    case 1:
-                        {
-                            comment.setAuthorEmail(commentLine);
-                            commentSwitch++;
-                            break;
-                        }
-                    case 2:
-                        {
-                            comment.setAuthorURL(commentLine);
-                            commentSwitch++;
-                            break;
-                        }
-                    default:
-                        {
-                            commentDescription.append(commentLine);
-                        }
-                }
-            }
-            comment.setComment(commentDescription.toString());
-            comment.setCommentDate(new Date(commentFile.lastModified()));
-            br.close();
-        } catch (IOException e) {
-            _logger.error(e);
-        }
-        return comment;
-    }
-
-    /**
      * Get the trackbacks
      *
      * @return ArrayList of trackbacks
@@ -502,98 +341,5 @@ public class BlogEntry implements BlojsomConstants {
         } else {
             return _trackbacks.size();
         }
-    }
-
-    /**
-     * Set the directory for trackbacks
-     *
-     * @param trackbacksDirectory Trackbacks directory
-     */
-    public void setTrackbacksDirectory(String trackbacksDirectory) {
-        _trackbacksDirectory = trackbacksDirectory;
-    }
-
-    /**
-     * Convenience method to load the trackbacks for this blog entry.
-     */
-    public void loadTrackbacks() {
-        String trackbacksDirectoryPath;
-        if (_source.getParent() == null) {
-            trackbacksDirectoryPath = File.separator + _trackbacksDirectory + File.separator + _source.getName();
-        } else {
-            trackbacksDirectoryPath = _source.getParent() + File.separator + _trackbacksDirectory + File.separator + _source.getName();
-        }
-        File trackbacksDirectory = new File(trackbacksDirectoryPath);
-        File[] trackbacks = trackbacksDirectory.listFiles(BlojsomUtils.getExtensionFilter(TRACKBACK_EXTENSION));
-        if ((trackbacks != null) && (trackbacks.length > 0)) {
-            _logger.debug("Adding " + trackbacks.length + " trackbacks to blog entry: " + getPermalink());
-            Arrays.sort(trackbacks, BlojsomUtils.FILE_TIME_ASCENDING_COMPARATOR);
-            _trackbacks = new ArrayList(trackbacks.length);
-            for (int i = 0; i < trackbacks.length; i++) {
-                File trackbackFile = trackbacks[i];
-                _trackbacks.add(loadTrackback(trackbackFile));
-            }
-        }
-    }
-
-    /**
-     * Load a trackback for this blog entry from disk
-     * Trackbacks must always have the form:<br />
-     * title<br>
-     * excerpt<br />
-     * url<br />
-     * blog_name
-     *
-     * @param trackbackFile Trackback file
-     * @return Trackback Trackback loaded from disk
-     */
-    private Trackback loadTrackback(File trackbackFile) {
-        int trackbackSwitch = 0;
-        Trackback trackback = new Trackback();
-        trackback.setTrackbackDateLong(trackbackFile.lastModified());
-        String trackbackLine;
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(trackbackFile), UTF8));
-            while (((trackbackLine = br.readLine()) != null) && (trackbackSwitch < 4)) {
-                switch (trackbackSwitch) {
-                    case 0:
-                        {
-                            if (!"".equals(trackbackLine.trim())) {
-                                trackback.setTitle(trackbackLine);
-                            }
-                            trackbackSwitch++;
-                            break;
-                        }
-                    case 1:
-                        {
-                            if (!"".equals(trackbackLine.trim())) {
-                                trackback.setExcerpt(trackbackLine);
-                            }
-                            trackbackSwitch++;
-                            break;
-                        }
-                    case 2:
-                        {
-                            if (!"".equals(trackbackLine.trim())) {
-                                trackback.setUrl(trackbackLine);
-                            }
-                            trackbackSwitch++;
-                            break;
-                        }
-                    case 3:
-                        {
-                            if (!"".equals(trackbackLine.trim())) {
-                                trackback.setBlogName(trackbackLine);
-                            }
-                            trackbackSwitch++;
-                            break;
-                        }
-                }
-            }
-            br.close();
-        } catch (IOException e) {
-            _logger.error(e);
-        }
-        return trackback;
     }
 }
