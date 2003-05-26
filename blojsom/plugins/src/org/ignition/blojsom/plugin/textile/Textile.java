36de12346bfec7c075e1ecd603f2a4ee21505985
intabulas <intabulas@28e03f91-8513-0410-9821-a6140ff45be5>
2003-05-26 23:06:49 +0000

    Cleanup

--- blojsom/plugins/src/org/ignition/blojsom/plugin/textile/Textile.java
+++ blojsom/plugins/src/org/ignition/blojsom/plugin/textile/Textile.java
@@ -1,8 +1,47 @@
+/**
+ * Copyright (c) 2003, David A. Czarnecki
+ * All rights reserved.
+ *
+ * Portions Copyright (c) 2003 by Mark Lussier
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the "David A. Czarnecki" and "blojsom" nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Products derived from this software may not be called "blojsom",
+ * nor may "blojsom" appear in their name, without prior written permission of
+ * David A. Czarnecki.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.ignition.blojsom.plugin.textile;
 
 import java.util.StringTokenizer;
 
-
+/**
+ * Textile
+ *
+ * @author Mark Lussier
+ * @version $Id: Textile.java,v 1.2 2003-05-26 23:06:49 intabulas Exp $
+ */
 public class Textile implements TextileConstants {
 
     public Textile() {
@@ -61,13 +100,13 @@ public class Textile implements TextileConstants {
         /**
          * double equal signs mean <notextile>
          */
-        content = content.replaceAll(EXP_DOUBLEQUOTE_MATCH,EXP_DOUBLEQUOTE_REPLACE);
+        content = content.replaceAll(EXP_DOUBLEQUOTE_MATCH, EXP_DOUBLEQUOTE_REPLACE);
 
 
         /**
          * image qtag
          */
-        content =content.replaceAll(EXP_IMAGE_QTAG_MATCH, EXP_IMAGE_QTAG_REPLACE);
+        content = content.replaceAll(EXP_IMAGE_QTAG_MATCH, EXP_IMAGE_QTAG_REPLACE);
 
         //# image with hyperlink
         //text = preg_replace("(<img.+ \\/>):(\\S+)","<a href=\"$2\">$1</a>",text);
@@ -75,17 +114,17 @@ public class Textile implements TextileConstants {
         /**
          *  hyperlink qtag
          */
-        content = content.replaceAll(EXP_HREF_QTAG_MATCH,EXP_HREF_QTAG_REPLACE);
+        content = content.replaceAll(EXP_HREF_QTAG_MATCH, EXP_HREF_QTAG_REPLACE);
 
 
         /**
          * loop through the array, replacing qtags with html
          */
 
-        for(int x = 0; x < EXP_PHRASE_MODIFIER_SOURCETAGS.length; x++){
+        for (int x = 0; x < EXP_PHRASE_MODIFIER_SOURCETAGS.length; x++) {
             content.replaceAll("(^|\\s|>)" + EXP_PHRASE_MODIFIER_SOURCETAGS[x] + "\\b(.+?)\\b([^\\w\\s]*?)"
-                     + EXP_PHRASE_MODIFIER_SOURCETAGS[x] + "([^\\w\\s]{0,2})(\\s|$)?"
-                    ,"$1<" + EXP_PHRASE_MODIFIER_REPLACETAGS[x] + ">$2$3</" + EXP_PHRASE_MODIFIER_REPLACETAGS[x] + ">$4");
+                               + EXP_PHRASE_MODIFIER_SOURCETAGS[x] + "([^\\w\\s]{0,2})(\\s|$)?"
+                               , "$1<" + EXP_PHRASE_MODIFIER_REPLACETAGS[x] + ">$2$3</" + EXP_PHRASE_MODIFIER_REPLACETAGS[x] + ">$4");
         }
 
 
@@ -94,19 +133,17 @@ public class Textile implements TextileConstants {
         /**
          * From the Origional Docs:
          * "some weird bs with underscores and \b word boundaries,
-		 * so we'll do those on their own"
+         * so we'll do those on their own"
          */
-        content = content.replaceAll(EXP_EMPHASIS_MATCH,EXP_EMPHASIS_REPLACE);
-        content = content.replaceAll(EXP_ITALICS_MATCH,EXP_ITALICS_REPLACE);
-        content = content.replaceAll(EXP_SUPERSCRIPT_MATCH,EXP_SUPERSCRIPT_REPLACE);
+        content = content.replaceAll(EXP_EMPHASIS_MATCH, EXP_EMPHASIS_REPLACE);
+        content = content.replaceAll(EXP_ITALICS_MATCH, EXP_ITALICS_REPLACE);
+        content = content.replaceAll(EXP_SUPERSCRIPT_MATCH, EXP_SUPERSCRIPT_REPLACE);
 
 
         /**
          * small problem with double quotes at the end of a string
          */
         content = content.replaceAll(EXP_EOL_DBL_QUOTES, " ");
-
-
 
 
         return content;
