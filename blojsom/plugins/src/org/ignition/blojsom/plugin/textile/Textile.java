b4d251d388aeda9c3c766c7c0307c5367e0ac51f
intabulas <intabulas@28e03f91-8513-0410-9821-a6140ff45be5>
2003-05-27 01:25:30 +0000

    Complete, but buggy implementation

--- blojsom/plugins/src/org/ignition/blojsom/plugin/textile/Textile.java
+++ blojsom/plugins/src/org/ignition/blojsom/plugin/textile/Textile.java
@@ -34,18 +34,22 @@
  */
 package org.ignition.blojsom.plugin.textile;
 
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * Textile
  *
  * @author Mark Lussier
- * @version $Id: Textile.java,v 1.2 2003-05-26 23:06:49 intabulas Exp $
+ * @version $Id: Textile.java,v 1.3 2003-05-27 01:25:09 intabulas Exp $
  */
 public class Textile implements TextileConstants {
 
+    /**
+     *
+     */
     public Textile() {
-
     }
 
 
@@ -130,6 +134,9 @@ public class Textile implements TextileConstants {
 
 
 
+
+
+
         /**
          * From the Origional Docs:
          * "some weird bs with underscores and \b word boundaries,
@@ -146,8 +153,247 @@ public class Textile implements TextileConstants {
         content = content.replaceAll(EXP_EOL_DBL_QUOTES, " ");
 
 
+        String[] glyphMatches = {EXP_SINGLE_CLOSING,
+                                 EXP_SINGLE_OPENING,
+                                 EXP_DOUBLE_CLOSING,
+                                 EXP_DOUBLE_OPENING,
+                                 EXP_ELLIPSES,
+                                 EXP_3UPPER_ACCRONYM,
+                                 EXP_3UPPERCASE_CAPS,
+                                 EXP_EM_DASH,
+                                 EXP_EN_DASH,
+                                 EXP_EN_DECIMAL_DASH,
+                                 EXP_DIMENSION_SIGN,
+                                 EXP_TRADEMARK,
+                                 EXP_REGISTERED,
+                                 EXP_COPYRIGHT};
+
+
+        String[] glyphReplacement = {REPLACE_SINGLE_CLOSING,
+                                     REPLACE_SINGLE_OPENING,
+                                     REPLACE_DOUBLE_CLOSING,
+                                     REPLACE_DOUBLE_OPENING,
+                                     REPLACE_ELLIPSES,
+                                     REPLACE_3UPPER_ACCRONYM,
+                                     REPLACE_3UPPERCASE_CAPS,
+                                     REPLACE_EM_DASH,
+                                     REPLACE_EN_DASH,
+                                     REPLACE_EN_DECIMAL_DASH,
+                                     REPLACE_DIMENSION_SIGN,
+                                     REPLACE_TRADEMARK,
+                                     REPLACE_REGISTERED,
+                                     REPLACE_COPYRIGHT};
+
+
+        boolean ishtml = Pattern.compile(EXP_ISHTML).matcher(content).find();
+        boolean inpreservation = false;
+
+        if (!ishtml) {
+            content = arrayReplaceAll(content, glyphMatches, glyphReplacement);
+        } else {
+            String[] segments = splitContent(EXP_ISHTML, content);
+
+            StringBuffer segmentBuffer = new StringBuffer();
+            for (int x = 0; x < segments.length; x++) {
+                //  # matches are off if we're between <code>, <pre> etc.
+                if (segments[x].toLowerCase().matches(EXP_STARTPRESERVE)) {
+                    inpreservation = true;
+                } else if (segments[x].toLowerCase().matches(EXP_ENDPRESERVE)) {
+                    inpreservation = true;
+                }
+
+                if (!Pattern.compile(EXP_ISHTML).matcher(segments[x]).find() && !inpreservation) {
+                    segments[x] = arrayReplaceAll(segments[x], glyphMatches, glyphReplacement);
+                }
+
+                //# convert htmlspecial if between <code>
+                if (inpreservation) {
+                    segments[x] = htmlSpecialChars(segments[x], MODE_ENT_NOQUOTES);
+                    segments[x] = replace(segments[x], "&lt;pre&gt;", "<pre>");
+                    segments[x] = replace(segments[x], "&lt;code&gt;", "<code>");
+                    segments[x] = replace(segments[x], "&lt;notextile&gt;", "<notextile>");
+                }
+
+                segmentBuffer.append(segments[x]);
+
+            }
+
+            content = segmentBuffer.toString();
+
+        }
+
+
+        //### Block level formatting
+
+        //# deal with forced breaks; this is going to be a problem between
+        //#  <pre> tags, but we'll clean them later
+
+        content = content.replaceAll(EXP_FORCESLINEBREAKS, REPLACE_FORCESLINEBREAK);
+
+        //# might be a problem with lists
+        content = replace(content, "l><br />", "l>\n");
+
+
+        String[] blockMatches = {EXP_BULLETED_LIST,
+                                 EXP_NUMERIC_LIST,
+                                 EXP_BLOCKQUOTE,
+                                 EXP_HEADER_WITHCLASS,
+                                 EXP_HEADER,
+                                 EXP_PARA_WITHCLASS,
+                                 EXP_PARA,
+                                 EXP_REMAINING_PARA};
+
+        String[] blockReplace = {REPLACE_BULLETED_LIST,
+                                 REPLACE_NUMERIC_LIST,
+                                 REPLACE_BLOCKQUOTE,
+                                 REPLACE_HEADER_WITHCLASS,
+                                 REPLACE_HEADER,
+                                 REPLACE_PARA_WITHCLASS,
+                                 REPLACE_PARA,
+                                 REPLACE_REMAINING_PARA};
+
+
+        StringBuffer blockBuffer = new StringBuffer();
+        String list = "";
+        content += " \n";
+
+        boolean inpre = false;
+        //# split the text into an array by newlines
+        StringTokenizer blockTokenizer = new StringTokenizer(content, "\n", false);
+
+        while (blockTokenizer.hasMoreTokens()) {
+            String line = blockTokenizer.nextToken();
+
+            //#make sure the line isn't blank
+            if (!line.matches("^$")) {
+
+                //# matches are off if we're between <pre> or <code> tags
+                if (line.toLowerCase().indexOf("<pre>") > -1) {
+                    inpre = true;
+                }
+
+                //# deal with block replacements first, then see if we're in a list
+                if (!inpre) {
+                    line = arrayReplaceAll(line, blockMatches, blockReplace);
+                }
+
+                //# kill any br tags that slipped in earlier
+                if (inpre) {
+                    line = replace(line, "<br />", "\n");
+                    line = replace(line, "<br/>", "\n");
+                }
+                //# matches back on after </pre>
+                if (line.toLowerCase().indexOf("</pre>") > -1) {
+                    inpre = false;
+                }
+
+                //# at the beginning of a list, $line switches to a value
+                boolean islist = Pattern.compile(EXP_LISTSTART).matcher(line).find();
+                boolean islistline = Pattern.compile(EXP_LISTSTART + list).matcher(line).find();
+                if (list.length() == 0 && islist) {
+                    line = line.replaceAll(EXP_MATCHLIST, REPLACE_MATCHLIST);
+                    list = line.substring(2, 3);
+
+                    //# at the end of a list, $line switches to empty
+                } else if (list.length() > 0 && !islistline) {
+                    line = line.replaceAll(EXP_ENDMATCHLIST, "</" + list + REPLACE_ENDMATCHLIST);
+                    list = "";
+                }
+            }
+            // push each line to a new array once it's processed
+            blockBuffer.append(line);
+            blockBuffer.append("\n");
+
+        }
+
+        content = blockBuffer.toString();
+
+
+        //#clean up <notextile>
+        content = content.replaceAll("<\\/?notextile>", "");
+
+        //# clean up liu and lio
+        content = content.replaceAll("<(\\/?)li(u|o)>", "<$1li>");
+
+        //# turn the temp char back to an ampersand entity
+        content = replace(content,"x%x%","&#38;");
+
+        //# Newline linebreaks, just for markup tidiness
+        content= replace(content,"<br />","<br />\n");
+
+
         return content;
 
+    }
+
+    /**
+     * An implementation of the PHP htmlspecialchars()
+     * @param content
+     * @param mode
+     * @return
+     */
+    private String htmlSpecialChars(String content, int mode) {
+
+        content = replace(content, "&", "&amp;");
+
+
+        if (mode != MODE_ENT_NOQUOTES) {
+            content = replace(content, "\"", "&quot;");
+        }
+        if (mode == MODE_ENT_QUOTES) {
+            content = replace(content, "'", "&#039;");
+        }
+        content = replace(content, "<", "&lt;");
+        content = replace(content, ">", "&gt;");
+        return content;
+
+    }
+
+
+    private String[] splitContent(String matchexp, String content) {
+
+        int startAt = 0;
+        List tempList = new ArrayList();
+
+        Pattern pattern = Pattern.compile(matchexp);
+
+        Matcher matcher = pattern.matcher(content);
+
+        while (matcher.find()) {
+            tempList.add(content.substring(startAt, matcher.start()));
+            tempList.add(matcher.group());
+            startAt = matcher.end();
+        }
+
+        tempList.add(content.substring(startAt));
+
+        String[] result = new String[tempList.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (String) tempList.get(i);
+        }
+
+        return result;
+
+    }
+
+
+    /**
+     *
+     * @param content
+     * @param matches
+     * @param replaces
+     * @return
+     */
+    private String arrayReplaceAll(String content, String[] matches, String[] replaces) {
+
+        String result = content;
+
+        for (int x = 0; x < matches.length; x++) {
+            result = result.replaceAll(matches[x], replaces[x]);
+        }
+
+        return result;
     }
 
 
--- blojsom/plugins/src/org/ignition/blojsom/plugin/textile/TextileConstants.java
+++ blojsom/plugins/src/org/ignition/blojsom/plugin/textile/TextileConstants.java
@@ -38,10 +38,16 @@ package org.ignition.blojsom.plugin.textile;
  * Textile Constants
  *
  * @author Mark Lussier
- * @version $Id: TextileConstants.java,v 1.2 2003-05-26 23:06:49 intabulas Exp $
+ * @version $Id: TextileConstants.java,v 1.3 2003-05-27 01:25:09 intabulas Exp $
  */
 public interface TextileConstants {
 
+    public static final int MODE_ENT_COMPAT = 0;
+    public static final int MODE_ENT_NOQUOTES = 2;
+    public static final int MODE_ENT_QUOTES = 3;
+
+
+    public static final String EXP_ISHTML = "<.*>";
 
     public static final String EXP_AMPERSAND = "&(?![#a-zA-Z0-9]+;)";
     public static final String EXP_AMPERSAND_REPLACE = "x%x%";
@@ -73,5 +79,70 @@ public interface TextileConstants {
 
     public static final String EXP_EOL_DBL_QUOTES = "\"$";
 
+
+    public static final String EXP_SINGLE_CLOSING = "\"([^\\\\']*)\\\\'([^\\\\']*)\"";
+    public static final String EXP_SINGLE_OPENING = "\\'";
+    public static final String EXP_DOUBLE_CLOSING = "([^\\']*)\\\"([^\\\"]*)";
+    public static final String EXP_DOUBLE_OPENING = "\"";
+    public static final String EXP_ELLIPSES = "\\b( )?\\.{3}";
+    public static final String EXP_3UPPER_ACCRONYM = "\\b([A-Z][A-Z0-9]{2,})\\b(\\(([^\\)]+)\\))";
+    public static final String EXP_3UPPERCASE_CAPS = "(^|[^\"][>\\s])([A-Z][A-Z0-9 ]{2,})([^<a-z0-9]|$)";
+    public static final String EXP_EM_DASH = "\\s?--\\s?";
+    public static final String EXP_EN_DASH = "\\s-\\s";
+    public static final String EXP_EN_DECIMAL_DASH = "(\\d+)-(\\d+)";
+    public static final String EXP_DIMENSION_SIGN = "(\\d+) ?x ?(\\d+)";
+    public static final String EXP_TRADEMARK = "\\b ?(\\((tm|TM)\\))";
+    public static final String EXP_REGISTERED = "\\b ?(\\([rR]\\))";
+    public static final String EXP_COPYRIGHT = "\\b ?(\\([cC]\\))";
+
+
+    public static final String REPLACE_SINGLE_CLOSING = "$1&#8217;$2";
+    public static final String REPLACE_SINGLE_OPENING = "&#8216;";
+    public static final String REPLACE_DOUBLE_CLOSING = "$1&#8221;";
+    public static final String REPLACE_DOUBLE_OPENING = "&#8220;";
+    public static final String REPLACE_ELLIPSES = "$1&#8230;";
+    public static final String REPLACE_3UPPER_ACCRONYM = "<acronym title=\"$2\">$1</acronym>";
+    public static final String REPLACE_3UPPERCASE_CAPS = "$1<span class=\"caps\">$2</span>$3";
+
+    public static final String REPLACE_EM_DASH = "&#8212;";
+    public static final String REPLACE_EN_DASH = " &#8211; ";
+    public static final String REPLACE_EN_DECIMAL_DASH = "$1&#8211;$2";
+    public static final String REPLACE_DIMENSION_SIGN = "$1&#215;$2";
+    public static final String REPLACE_TRADEMARK = "&#8482;";
+    public static final String REPLACE_REGISTERED = "&#174;";
+    public static final String REPLACE_COPYRIGHT = "&#169;";
+
+
+    public static final String EXP_STARTPRESERVE = "<(code|pre|kbd|notextile)>";
+    public static final String EXP_ENDPRESERVE = "</(code|pre|kbd|notextile)>";
+
+    public static final String EXP_FORCESLINEBREAKS = "(\\S)(_*)([:punct:]*) *\\n([^#*\\s])";
+    public static final String REPLACE_FORCESLINEBREAK = "$1$2$3<br />$4";
+
+
+    public static final String EXP_BULLETED_LIST = "^\\s?\\*\\s(.*)$";
+    public static final String EXP_NUMERIC_LIST = "^\\s?#\\s(.*)$";
+    public static final String EXP_BLOCKQUOTE = "^bq\\. (.*)";
+    public static final String EXP_HEADER_WITHCLASS = "^h(\\d)\\(([\\w]+)\\)\\.\\s(.*)";
+    public static final String EXP_HEADER = "^h(\\d)\\. (.*)";
+    public static final String EXP_PARA_WITHCLASS = "^p\\(([\\w]+)\\)\\.\\s(.*)";
+    public static final String EXP_PARA = "^p\\. (.*)";
+    public static final String EXP_REMAINING_PARA = "^([^\\t ]+.*)";
+
+
+    public static final String REPLACE_BULLETED_LIST = "\t<liu>$1</liu>";
+    public static final String REPLACE_NUMERIC_LIST = "\t<lio>$1</lio>";
+    public static final String REPLACE_BLOCKQUOTE = "\t<blockquote>$1</blockquote>";
+    public static final String REPLACE_HEADER_WITHCLASS = "\t<h$1 class=\"$2\">$3</h$1>";
+    public static final String REPLACE_HEADER = "\t<h$1>$2</h$1>";
+    public static final String REPLACE_PARA_WITHCLASS = "\t<p class=\"$1\">$2</p>";
+    public static final String REPLACE_PARA = "\t<p>$1</p>";
+    public static final String REPLACE_REMAINING_PARA = "\t<p>$1</p>";
+
+    public static final String EXP_LISTSTART = "\\t<li";
+    public static final String EXP_MATCHLIST = "^(\\t<li)(o|u)";
+    public static final String REPLACE_MATCHLIST = "\n<$2l>\n$1$2";
+    public static final String EXP_ENDMATCHLIST = "^(.*)$";
+    public static final String REPLACE_ENDMATCHLIST = "l>\n$1";
 
 }
