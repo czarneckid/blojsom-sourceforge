9ee832787986c5e9d2fc42529f2d1566719a5278
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-06-18 04:40:35 +0000

    Added comments.

--- blojsom/plugins/src/org/ignition/blojsom/plugin/weblogsping/WeblogsPingPlugin.java
+++ blojsom/plugins/src/org/ignition/blojsom/plugin/weblogsping/WeblogsPingPlugin.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
  *
  * @author David Czarnecki
  * @since blojsom 1.9.2
- * @version $Id: WeblogsPingPlugin.java,v 1.2 2003-06-18 04:33:10 czarneckid Exp $
+ * @version $Id: WeblogsPingPlugin.java,v 1.3 2003-06-18 04:40:35 czarneckid Exp $
  */
 public class WeblogsPingPlugin implements BlojsomPlugin {
 
@@ -34,6 +34,7 @@ public class WeblogsPingPlugin implements BlojsomPlugin {
 
     private static final String WEBLOGS_PING_METADATA = "weblogs-ping";
     private static final String WEBLOGS_PING_VALUE = "true";
+    private static final String WEBLOGS_PING_METHOD = "weblogUpdates.ping";
     private Blog _blog;
     private WeblogsPingPluginAsyncCallback _callbackHandler;
 
@@ -69,13 +70,19 @@ public class WeblogsPingPlugin implements BlojsomPlugin {
                                Map context, BlogEntry[] entries) throws BlojsomPluginException {
 
         boolean shouldPingWeblogs = false;
+
+        // Loop through all the entries
         for (int i = 0; i < entries.length; i++) {
             BlogEntry entry = entries[i];
             Map entryMetaData = entry.getMetaData();
             if (entryMetaData == null) {
                 entryMetaData = new HashMap();
             }
-            if (!entryMetaData.containsKey(WEBLOGS_PING_METADATA)) {
+
+            // In the default FileBackedBlogEntry, supportComments indicates the blog entry
+            // is writable. So, for now, use this to see if we should check for the meta-data.
+            // If not available, set it on the blog entry and save the blog entry.
+            if (entry.supportsComments() && !entryMetaData.containsKey(WEBLOGS_PING_METADATA)) {
                 shouldPingWeblogs = true;
                 entryMetaData.put(WEBLOGS_PING_METADATA, WEBLOGS_PING_VALUE);
                 entry.setMetaData(entryMetaData);
@@ -85,17 +92,19 @@ public class WeblogsPingPlugin implements BlojsomPlugin {
                     _logger.error("Error saving blog entry after adding weblogs-ping meta-data.", e);
                 }
             }
+        }
 
-            if (shouldPingWeblogs) {
-                try {
-                    XmlRpcClient client = new XmlRpcClient(DEFAULT_WEBLOGS_URL);
-                    Vector params = new Vector();
-                    params.add(_blog.getBlogName());
-                    params.add(_blog.getBlogURL());
-                    client.executeAsync("weblogUpdates.ping", params, _callbackHandler);
-                } catch (IOException e) {
-                    _logger.error(e);
-                }
+        // If one (or more) of the entries has its meta-data set for pinging weblogs.com, then
+        // go ahead and do an asynchronous call to weblogs.com.
+        if (shouldPingWeblogs) {
+            try {
+                XmlRpcClient client = new XmlRpcClient(DEFAULT_WEBLOGS_URL);
+                Vector params = new Vector();
+                params.add(_blog.getBlogName());
+                params.add(_blog.getBlogURL());
+                client.executeAsync(WEBLOGS_PING_METHOD, params, _callbackHandler);
+            } catch (IOException e) {
+                _logger.error(e);
             }
         }
 
