1dbc2739805773eba8a2cdd43ecd56f176a985d4
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-09-24 03:42:46 +0000

    Added copyright.

--- blojsom-2.0/addon/plugins/src/org/blojsom/plugin/xpath/BlogEntryWrapper.java
+++ blojsom-2.0/addon/plugins/src/org/blojsom/plugin/xpath/BlogEntryWrapper.java
@@ -1,3 +1,37 @@
+/**
+ * Copyright (c) 2003, David A. Czarnecki
+ * All rights reserved.
+ *
+ * Portions Copyright (c) 2003 by Mark Lussier
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the "David A. Czarnecki" and "blojsom" nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Products derived from this software may not be called "blojsom",
+ * nor may "blojsom" appear in their name, without prior written permission of
+ * David A. Czarnecki.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.blojsom.plugin.xpath;
 
 import org.blojsom.blog.BlogEntry;
@@ -7,7 +41,8 @@ import org.blojsom.blog.BlogEntry;
  * over BlogEntry array's.
  *
  * @author Mark Lussier
- * @version $Id: BlogEntryWrapper.java,v 1.1 2003-09-24 03:20:34 intabulas Exp $
+ * @since blojsom 2.02
+ * @version $Id: BlogEntryWrapper.java,v 1.2 2003-09-24 03:42:46 czarneckid Exp $
  */
 
 public class BlogEntryWrapper {
