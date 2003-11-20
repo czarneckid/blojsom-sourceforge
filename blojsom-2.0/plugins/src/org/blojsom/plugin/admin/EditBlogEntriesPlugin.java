ae3ce4946d1d7f461f278ea0c035397ad1e77309
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-11-20 03:02:02 +0000

    Added plugin to edit blog entries via the web.

---
+++ blojsom-2.0/plugins/src/org/blojsom/plugin/admin/EditBlogEntriesPlugin.java
@@ -0,0 +1,251 @@
+/**
+ * Copyright (c) 2003, David A. Czarnecki
+ * All rights reserved.
+ *
+ * Portions Copyright (c) 2003 by Mark Lussier
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the "David A. Czarnecki" and "blojsom" nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Products derived from this software may not be called "blojsom",
+ * nor may "blojsom" appear in their name, without prior written permission of
+ * David A. Czarnecki.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.blojsom.plugin.admin;
+
+import org.blojsom.blog.BlogCategory;
+import org.blojsom.blog.BlogEntry;
+import org.blojsom.blog.BlogUser;
+import org.blojsom.blog.BlojsomConfiguration;
+import org.blojsom.fetcher.BlojsomFetcher;
+import org.blojsom.fetcher.BlojsomFetcherException;
+import org.blojsom.plugin.BlojsomPluginException;
+import org.blojsom.util.BlojsomUtils;
+import org.blojsom.BlojsomException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * EditBlogEntriesPlugin
+ *
+ * @author czarnecki
+ * @since blojsom 2.05
+ * @version $Id: EditBlogEntriesPlugin.java,v 1.1 2003-11-20 03:02:02 czarneckid Exp $
+ */
+public class EditBlogEntriesPlugin extends BaseAdminPlugin {
+
+    private static final Log _logger = LogFactory.getLog(EditBlogEntriesPlugin.class);
+
+    // Pages
+    private static final String EDIT_BLOG_ENTRIES_PAGE = "/org/blojsom/plugin/admin/templates/admin-edit-blog-entries";
+    private static final String EDIT_BLOG_ENTRIES_LIST_PAGE = "/org/blojsom/plugin/admin/templates/admin-edit-blog-entries-list";
+    private static final String EDIT_BLOG_ENTRY_PAGE = "/org/blojsom/plugin/admin/templates/admin-edit-blog-entry";
+
+    // Constants
+    private static final String BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_LIST = "BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_LIST";
+    private static final String BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_CATEGORY = "BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_CATEGORY";
+    private static final String BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_ENTRY = "BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_ENTRY";
+
+    // Actions
+    private static final String EDIT_BLOG_ENTRIES_ACTION = "edit-blog-entries";
+    private static final String EDIT_BLOG_ENTRY_ACTION = "edit-blog-entry";
+    private static final String UPDATE_BLOG_ENTRY_ACTION = "update-blog-entry";
+
+    // Form elements
+    private static final String BLOG_CATEGORY_NAME = "blog-category-name";
+    private static final String BLOG_ENTRY_ID = "blog-entry-id";
+    private static final String BLOG_ENTRY_TITLE = "blog-entry-title";
+    private static final String BLOG_ENTRY_DESCRIPTION = "blog-entry-description";
+
+    private BlojsomFetcher _fetcher;
+
+    /**
+     * Default constructor.
+     */
+    public EditBlogEntriesPlugin() {
+    }
+
+    /**
+     * Initialize this plugin. This method only called when the plugin is instantiated.
+     *
+     * @param servletConfig Servlet config object for the plugin to retrieve any initialization parameters
+     * @param blojsomConfiguration {@link BlojsomConfiguration} information
+     * @throws org.blojsom.plugin.BlojsomPluginException If there is an error initializing the plugin
+     */
+    public void init(ServletConfig servletConfig, BlojsomConfiguration blojsomConfiguration) throws BlojsomPluginException {
+        super.init(servletConfig, blojsomConfiguration);
+
+        String fetcherClassName = blojsomConfiguration.getFetcherClass();
+        try {
+            Class fetcherClass = Class.forName(fetcherClassName);
+            _fetcher = (BlojsomFetcher) fetcherClass.newInstance();
+            _fetcher.init(servletConfig, blojsomConfiguration);
+            _logger.info("Added blojsom fetcher: " + fetcherClassName);
+        } catch (ClassNotFoundException e) {
+            _logger.error(e);
+            throw new BlojsomPluginException(e);
+        } catch (InstantiationException e) {
+            _logger.error(e);
+            throw new BlojsomPluginException(e);
+        } catch (IllegalAccessException e) {
+            _logger.error(e);
+            throw new BlojsomPluginException(e);
+        } catch (BlojsomFetcherException e) {
+            _logger.error(e);
+            throw new BlojsomPluginException(e);
+        }
+    }
+
+    /**
+     * Process the blog entries
+     *
+     * @param httpServletRequest  Request
+     * @param httpServletResponse Response
+     * @param user                {@link org.blojsom.blog.BlogUser} instance
+     * @param context             Context
+     * @param entries             Blog entries retrieved for the particular request
+     * @return Modified set of blog entries
+     * @throws BlojsomPluginException If there is an error processing the blog entries
+     */
+    public BlogEntry[] process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlogUser user, Map context, BlogEntry[] entries) throws BlojsomPluginException {
+        entries = super.process(httpServletRequest, httpServletResponse, user, context, entries);
+
+        String action = BlojsomUtils.getRequestValue(ACTION_PARAM, httpServletRequest);
+        if (action == null || "".equals(action)) {
+            _logger.debug("User did not request edit action");
+            httpServletRequest.setAttribute(PAGE_PARAM, ADMIN_ADMINISTRATION_PAGE);
+        } else if (PAGE_ACTION.equals(action)) {
+            _logger.debug("User requested edit blog entries page");
+
+            httpServletRequest.setAttribute(PAGE_PARAM, EDIT_BLOG_ENTRIES_PAGE);
+        } else if (EDIT_BLOG_ENTRIES_ACTION.equals(action)) {
+            _logger.debug("User requested edit blog entries list page");
+
+            String blogCategoryName = BlojsomUtils.getRequestValue(BLOG_CATEGORY_NAME, httpServletRequest);
+            blogCategoryName = BlojsomUtils.normalize(blogCategoryName);
+
+            BlogCategory category;
+            category = _fetcher.newBlogCategory();
+            category.setCategory(blogCategoryName);
+            category.setCategoryURL(user.getBlog().getBlogURL() + BlojsomUtils.removeInitialSlash(blogCategoryName));
+
+            Map fetchMap = new HashMap();
+            fetchMap.put(BlojsomFetcher.FETCHER_CATEGORY, category);
+            fetchMap.put(BlojsomFetcher.FETCHER_NUM_POSTS_INTEGER, new Integer(-1));
+            try {
+                entries = _fetcher.fetchEntries(fetchMap, user);
+                if (entries != null) {
+                    _logger.debug("Retrieved " + entries.length + " entries from category: " + blogCategoryName);
+                    Arrays.sort(entries, BlojsomUtils.FILE_TIME_COMPARATOR);
+                } else {
+                    _logger.debug("No entries found in category: " + blogCategoryName);
+                }
+            } catch (BlojsomFetcherException e) {
+                _logger.error(e);
+                entries = new BlogEntry[0];
+            }
+
+            context.put(BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_CATEGORY, blogCategoryName);
+            context.put(BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_LIST, entries);
+            httpServletRequest.setAttribute(PAGE_PARAM, EDIT_BLOG_ENTRIES_LIST_PAGE);
+        } else if (EDIT_BLOG_ENTRY_ACTION.equals(action)) {
+            _logger.debug("User requested edit blog entry action");
+
+            String blogCategoryName = BlojsomUtils.getRequestValue(BLOG_CATEGORY_NAME, httpServletRequest);
+            blogCategoryName = BlojsomUtils.normalize(blogCategoryName);
+            String blogEntryId = BlojsomUtils.getRequestValue(BLOG_ENTRY_ID, httpServletRequest);
+            _logger.debug("Blog entry id: " + blogEntryId);
+
+            BlogCategory category;
+            category = _fetcher.newBlogCategory();
+            category.setCategory(blogCategoryName);
+            category.setCategoryURL(user.getBlog().getBlogURL() + BlojsomUtils.removeInitialSlash(blogCategoryName));
+
+            Map fetchMap = new HashMap();
+            fetchMap.put(BlojsomFetcher.FETCHER_CATEGORY, category);
+            fetchMap.put(BlojsomFetcher.FETCHER_PERMALINK, blogEntryId);
+            try {
+                entries = _fetcher.fetchEntries(fetchMap, user);
+                if (entries != null) {
+                    _logger.debug("Retrieved " + entries.length + " entries from category: " + blogCategoryName);
+                    context.put(BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_ENTRY, entries[0]);
+                } else {
+                    _logger.debug("No entries found in category: " + blogCategoryName);
+                }
+            } catch (BlojsomFetcherException e) {
+                _logger.error(e);
+                entries = new BlogEntry[0];
+            }
+
+            context.put(BLOJSOM_PLUGIN_EDIT_BLOG_ENTRIES_CATEGORY, blogCategoryName);
+            httpServletRequest.setAttribute(PAGE_PARAM, EDIT_BLOG_ENTRY_PAGE);
+        } else if (UPDATE_BLOG_ENTRY_ACTION.equals(action)) {
+            _logger.debug("User requested update blog entry action");
+
+            String blogCategoryName = BlojsomUtils.getRequestValue(BLOG_CATEGORY_NAME, httpServletRequest);
+            blogCategoryName = BlojsomUtils.normalize(blogCategoryName);
+            String blogEntryId = BlojsomUtils.getRequestValue(BLOG_ENTRY_ID, httpServletRequest);
+            String blogEntryDescription = BlojsomUtils.getRequestValue(BLOG_ENTRY_DESCRIPTION, httpServletRequest);
+            String blogEntryTitle = BlojsomUtils.getRequestValue(BLOG_ENTRY_TITLE, httpServletRequest);
+            _logger.debug("Blog entry id: " + blogEntryId);
+
+            BlogCategory category;
+            category = _fetcher.newBlogCategory();
+            category.setCategory(blogCategoryName);
+            category.setCategoryURL(user.getBlog().getBlogURL() + BlojsomUtils.removeInitialSlash(blogCategoryName));
+
+            Map fetchMap = new HashMap();
+            fetchMap.put(BlojsomFetcher.FETCHER_CATEGORY, category);
+            fetchMap.put(BlojsomFetcher.FETCHER_PERMALINK, blogEntryId);
+            try {
+                entries = _fetcher.fetchEntries(fetchMap, user);
+                if (entries != null) {
+                    _logger.debug("Retrieved " + entries.length + " entries from category: " + blogCategoryName);
+                    BlogEntry entryToUpdate = entries[0];
+                    entryToUpdate.setTitle(blogEntryTitle);
+                    entryToUpdate.setDescription(blogEntryDescription);
+                    entryToUpdate.save(user.getBlog());
+                    _logger.debug("Updated blog entry: " + entryToUpdate.getLink());
+                } else {
+                    _logger.debug("No entries found in category: " + blogCategoryName);
+                }
+            } catch (BlojsomFetcherException e) {
+                _logger.error(e);
+                entries = new BlogEntry[0];
+            } catch (BlojsomException e) {
+                _logger.error(e);
+                entries = new BlogEntry[0];
+            }
+        }
+
+        return entries;
+    }
+}
