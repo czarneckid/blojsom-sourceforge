01d4826e52bb79cb7cbccf8c3b23d1d6fe29e597
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-12-18 17:43:02 +0000

    Added persistence of flavor configuration file. Users can now add, modify, and delete flavors.

--- blojsom-2.0/plugins/src/org/blojsom/plugin/admin/EditBlogFlavorsPlugin.java
+++ blojsom-2.0/plugins/src/org/blojsom/plugin/admin/EditBlogFlavorsPlugin.java
@@ -39,6 +39,7 @@ import org.blojsom.blog.BlojsomConfiguration;
 import org.blojsom.blog.BlogEntry;
 import org.blojsom.blog.BlogUser;
 import org.blojsom.util.BlojsomUtils;
+import org.blojsom.util.BlojsomProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -47,39 +48,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Properties;
 import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
 /**
  * EditBlogFlavorsPlugin
  * 
  * @author czarnecki
+ * @version $Id: EditBlogFlavorsPlugin.java,v 1.3 2003-12-18 17:43:02 czarneckid Exp $
  * @since blojsom 2.05
- * @version $Id: EditBlogFlavorsPlugin.java,v 1.2 2003-12-18 02:21:50 czarneckid Exp $
  */
 public class EditBlogFlavorsPlugin extends BaseAdminPlugin {
 
     private Log _logger = LogFactory.getLog(EditBlogFlavorsPlugin.class);
 
-    private String _templatesDirectory;
-    private String _installationDirectory;
-    private String _baseConfigurationDirectory;
-
     // Pages
-    private static final String ADD_BLOG_FLAVOR_PAGE = "/org/blojsom/plugin/admin/templates/admin-add-blog-flavor";
+    private static final String EDIT_BLOG_FLAVORS_PAGE = "/org/blojsom/plugin/admin/templates/admin-edit-blog-flavors";
 
     // Constants
     private static final String DEFAULT_MIME_TYPE = "text/html";
     private static final String DEFAULT_CHARACTER_SET = UTF8;
     private static final String BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_TEMPLATE_FILES = "BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_TEMPLATE_FILES";
+    private static final String BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_FLAVORS = "BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_FLAVORS";
 
     // Actions
     private static final String ADD_BLOG_FLAVOR_ACTION = "add-blog-flavor";
+    private static final String MODIFY_BLOG_FLAVOR_ACTION = "modify-blog-flavor";
+    private static final String DELETE_BLOG_FLAVOR_ACTION = "delete-blog-flavor";
 
     // Form elements
     private static final String FLAVOR_NAME = "flavor-name";
     private static final String FLAVOR_MIME_TYPE = "flavor-mime-type";
     private static final String FLAVOR_CHARACTER_SET = "flavor-character-set";
     private static final String BLOG_TEMPLATE = "blog-template";
+
+    private BlojsomConfiguration _blojsomConfiguration;
+    private String _flavorConfiguration;
 
     /**
      * Default constructor.
@@ -89,22 +96,22 @@ public class EditBlogFlavorsPlugin extends BaseAdminPlugin {
 
     /**
      * Initialize this plugin. This method only called when the plugin is instantiated.
-     *
+     * 
      * @param servletConfig        Servlet config object for the plugin to retrieve any initialization parameters
      * @param blojsomConfiguration {@link org.blojsom.blog.BlojsomConfiguration} information
-     * @throws org.blojsom.plugin.BlojsomPluginException If there is an error initializing the plugin
+     * @throws org.blojsom.plugin.BlojsomPluginException
+     *          If there is an error initializing the plugin
      */
     public void init(ServletConfig servletConfig, BlojsomConfiguration blojsomConfiguration) throws BlojsomPluginException {
         super.init(servletConfig, blojsomConfiguration);
 
-        _templatesDirectory = blojsomConfiguration.getTemplatesDirectory();
-        _installationDirectory = blojsomConfiguration.getInstallationDirectory();
-        _baseConfigurationDirectory = blojsomConfiguration.getBaseConfigurationDirectory();
+        _blojsomConfiguration = blojsomConfiguration;
+        _flavorConfiguration = servletConfig.getInitParameter(BLOJSOM_FLAVOR_CONFIGURATION_IP);
     }
 
     /**
      * Process the blog entries
-     *
+     * 
      * @param httpServletRequest  Request
      * @param httpServletResponse Response
      * @param user                {@link org.blojsom.blog.BlogUser} instance
@@ -116,73 +123,128 @@ public class EditBlogFlavorsPlugin extends BaseAdminPlugin {
     public BlogEntry[] process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlogUser user, Map context, BlogEntry[] entries) throws BlojsomPluginException {
         entries = super.process(httpServletRequest, httpServletResponse, user, context, entries);
 
+        // Put the available flavors in the context for the edit flavors template
+        context.put(BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_FLAVORS, user.getFlavors());
+
+        // Put the available templates in the context for the edit flavors template
+        File templatesDirectory = new File(_blojsomConfiguration.getInstallationDirectory() + BlojsomUtils.removeInitialSlash(_blojsomConfiguration.getBaseConfigurationDirectory()) +
+                user.getId() + _blojsomConfiguration.getTemplatesDirectory());
+        _logger.debug("Looking for templates in directory: " + templatesDirectory.toString());
+
+        File[] templates = templatesDirectory.listFiles();
+        ArrayList templatesList = new ArrayList(templates.length);
+        for (int i = 0; i < templates.length; i++) {
+            File template = templates[i];
+            if (template.isFile()) {
+                templatesList.add(template.getName());
+                _logger.debug("Added template: " + template.getName());
+            }
+        }
+        context.put(BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_TEMPLATE_FILES, templatesList);
+
         String action = BlojsomUtils.getRequestValue(ACTION_PARAM, httpServletRequest);
-        if (action == null || "".equals(action)) {
+        if (BlojsomUtils.checkNullOrBlank(action)) {
             _logger.debug("User did not request edit action");
+
             httpServletRequest.setAttribute(PAGE_PARAM, ADMIN_ADMINISTRATION_PAGE);
         } else if (PAGE_ACTION.equals(action)) {
             _logger.debug("User requested add blog flavor page");
 
-            File templatesDirectory = new File(_installationDirectory + BlojsomUtils.removeInitialSlash(_baseConfigurationDirectory) +
-                    user.getId() + _templatesDirectory);
-            _logger.debug("Looking for templates in directory: " + templatesDirectory.toString());
-
-            File[] templates = templatesDirectory.listFiles();
-            ArrayList templatesList = new ArrayList(templates.length);
-            for (int i = 0; i < templates.length; i++) {
-                File template = templates[i];
-                if (template.isFile()) {
-                    templatesList.add(template.getName());
-                    _logger.debug("Added template: " + template.getName());
-                }
-            }
-
-            context.put(BLOJSOM_PLUGIN_EDIT_BLOG_FLAVORS_TEMPLATE_FILES, templatesList);
-
-            httpServletRequest.setAttribute(PAGE_PARAM, ADD_BLOG_FLAVOR_PAGE);
-        } else if (ADD_BLOG_FLAVOR_ACTION.equals(action)) {
+            httpServletRequest.setAttribute(PAGE_PARAM, EDIT_BLOG_FLAVORS_PAGE);
+        } else if (ADD_BLOG_FLAVOR_ACTION.equals(action) || MODIFY_BLOG_FLAVOR_ACTION.equals(action)) {
             _logger.debug("User requested add blog flavor action");
 
             String flavorName = BlojsomUtils.getRequestValue(FLAVOR_NAME, httpServletRequest);
-            if (flavorName == null || "".equals(flavorName)) {
+            if (BlojsomUtils.checkNullOrBlank(flavorName)) {
                 _logger.debug("No flavor name specified");
                 return entries;
             }
 
             String blogTemplate = BlojsomUtils.getRequestValue(BLOG_TEMPLATE, httpServletRequest);
-            if (blogTemplate == null || "".equals(blogTemplate)) {
+            if (BlojsomUtils.checkNullOrBlank(blogTemplate)) {
                 _logger.debug("No blog template specified");
                 return entries;
             }
 
             String flavorMimeType = BlojsomUtils.getRequestValue(FLAVOR_MIME_TYPE, httpServletRequest);
-            if (flavorMimeType == null || "".equals(flavorMimeType)) {
+            if (BlojsomUtils.checkNullOrBlank(flavorMimeType)) {
                 flavorMimeType = DEFAULT_MIME_TYPE;
                 _logger.debug("Flavor MIME type not specified. Using default: " + flavorMimeType);
             }
+
             String flavorCharacterSet = BlojsomUtils.getRequestValue(FLAVOR_CHARACTER_SET, httpServletRequest);
-            if (flavorCharacterSet == null || "".equals(flavorCharacterSet)) {
+            if (BlojsomUtils.checkNullOrBlank(flavorCharacterSet)) {
                 flavorCharacterSet = DEFAULT_CHARACTER_SET;
                 _logger.debug("Flavor character set not specified. Using default: " + flavorCharacterSet);
             }
 
             Map flavorMapForUser = user.getFlavors();
-            if (flavorMapForUser.containsKey(flavorName)) {
-                _logger.debug("Flavor already exists: " + flavorName);
-                return entries;
-            } else {
-                Map flavorTemplatesForUser = user.getFlavorToTemplate();
-                Map flavorContentTypesForUser = user.getFlavorToContentType();
 
-                flavorMapForUser.put(flavorName, flavorName);
-                flavorTemplatesForUser.put(flavorName, blogTemplate);
-                flavorContentTypesForUser.put(flavorName, flavorMimeType + ";" + flavorCharacterSet);
-                _logger.debug("Successfully added flavor: " + flavorName + " using template: " + blogTemplate + " with content type: " + flavorMimeType + ";" + flavorCharacterSet);
+            Map flavorTemplatesForUser = user.getFlavorToTemplate();
+            Map flavorContentTypesForUser = user.getFlavorToContentType();
 
-                httpServletRequest.setAttribute(PAGE_PARAM, ADMIN_ADMINISTRATION_PAGE);
+            flavorMapForUser.put(flavorName, flavorName);
+            flavorTemplatesForUser.put(flavorName, blogTemplate);
+            flavorContentTypesForUser.put(flavorName, flavorMimeType + ";charset=" + flavorCharacterSet);
+            _logger.debug("Successfully added flavor: " + flavorName + " using template: " + blogTemplate + " with content type: " + flavorMimeType + ";" + flavorCharacterSet);
+
+            try {
+                writeFlavorConfiguration(user);
+                _logger.debug("Successfully wrote flavor configuration file for user: " + user.getId());
+            } catch (IOException e) {
+                _logger.error(e);
             }
 
+            httpServletRequest.setAttribute(PAGE_PARAM, EDIT_BLOG_FLAVORS_PAGE);
+        } else if (DELETE_BLOG_FLAVOR_ACTION.equals(action)) {
+            _logger.debug("User requested delete blog flavor action");
+
+            String flavorName = BlojsomUtils.getRequestValue(FLAVOR_NAME, httpServletRequest);
+            if (BlojsomUtils.checkNullOrBlank(flavorName)) {
+                _logger.debug("No flavor name specified");
+                return entries;
+            }
+
+            // Delete the flavor from the maps
+            user.getFlavors().remove(flavorName);
+            user.getFlavorToContentType().remove(flavorName);
+            user.getFlavorToTemplate().remove(flavorName);
+
+            // Write out the flavor configuration file
+            try {
+                writeFlavorConfiguration(user);
+                _logger.debug("Successfully wrote flavor configuration file for user: " + user.getId());
+            } catch (IOException e) {
+                _logger.error(e);
+            }
+
+            httpServletRequest.setAttribute(PAGE_PARAM, EDIT_BLOG_FLAVORS_PAGE);
         }
+
         return entries;
+    }
+
+    /**
+     * Write out the flavor configuration file for a particular user
+     * 
+     * @param user Blog user information
+     * @throws IOException If there is an error writing the flavor configuration file
+     */
+    private void writeFlavorConfiguration(BlogUser user) throws IOException {
+        Iterator flavorIterator = user.getFlavors().keySet().iterator();
+        Properties flavorProperties = new BlojsomProperties();
+        Map flavorToTemplateMap = user.getFlavorToTemplate();
+        Map flavorToContentTypeMap = user.getFlavorToContentType();
+
+        while (flavorIterator.hasNext()) {
+            String flavor = (String) flavorIterator.next();
+
+            flavorProperties.setProperty(flavor, flavorToTemplateMap.get(flavor) + ", " + flavorToContentTypeMap.get(flavor));
+        }
+
+        File flavorFile = new File(_blojsomConfiguration.getInstallationDirectory() + _blojsomConfiguration.getBaseConfigurationDirectory() + user.getId() + "/" + _flavorConfiguration);
+        FileOutputStream fos = new FileOutputStream(flavorFile);
+        flavorProperties.store(fos, null);
+        fos.close();
     }
 }
