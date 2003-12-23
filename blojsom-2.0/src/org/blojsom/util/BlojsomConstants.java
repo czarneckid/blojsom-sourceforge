b2f52a1169320b5f2a3105ffa4b7c2d33d0fb791
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2003-12-23 01:56:41 +0000

    Updated version to 2.07.

--- blojsom-2.0/src/org/blojsom/util/BlojsomConstants.java
+++ blojsom-2.0/src/org/blojsom/util/BlojsomConstants.java
@@ -40,14 +40,14 @@ package org.blojsom.util;
  * @author David Czarnecki
  * @author Mark Lussier
  * @author Dan Morrill
- * @version $Id: BlojsomConstants.java,v 1.20 2003-12-20 18:10:43 czarneckid Exp $
+ * @version $Id: BlojsomConstants.java,v 1.21 2003-12-23 01:56:41 czarneckid Exp $
  */
 public interface BlojsomConstants {
 
     /**
      * blojsom version
      */
-    public static final String BLOJSOM_VERSION_NUMBER = "blojsom v2.06";
+    public static final String BLOJSOM_VERSION_NUMBER = "blojsom v2.07";
 
     /**
      * Key under which blog information will be placed
