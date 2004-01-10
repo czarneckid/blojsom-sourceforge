c4741bb1dee2a902baddd96fcc7ecb4b1f7a3e31
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2004-01-10 15:52:58 +0000

    Fixed encoding of category when setting link to blog entry.

--- blojsom-2.0/src/org/blojsom/fetcher/StandardFetcher.java
+++ blojsom-2.0/src/org/blojsom/fetcher/StandardFetcher.java
@@ -55,7 +55,7 @@ import java.util.StringTokenizer;
  *
  * @author David Czarnecki
  * @since blojsom 1.8
- * @version $Id: StandardFetcher.java,v 1.8 2004-01-09 04:00:42 czarneckid Exp $
+ * @version $Id: StandardFetcher.java,v 1.9 2004-01-10 15:52:58 czarneckid Exp $
  */
 public class StandardFetcher implements BlojsomFetcher, BlojsomConstants {
 
@@ -148,7 +148,7 @@ public class StandardFetcher implements BlojsomFetcher, BlojsomConstants {
             if ("/".equals(category)) {
                 blogEntry.setLink(blog.getBlogURL() + '?' + PERMALINK_PARAM + '=' + BlojsomUtils.urlEncode(blogFile.getName()));
             } else {
-                blogEntry.setLink(blog.getBlogURL() + BlojsomUtils.urlEncode(category) + "?" + PERMALINK_PARAM + '=' + BlojsomUtils.urlEncode(blogFile.getName()));
+                blogEntry.setLink(blog.getBlogURL() + BlojsomUtils.urlEncode(category.substring(0, category.length() - 1)) + "/?" + PERMALINK_PARAM + '=' + BlojsomUtils.urlEncode(blogFile.getName()));
             }
             blogEntry.setBlogFileEncoding(blog.getBlogFileEncoding());
             blogEntry.setBlogCategory(blogCategory);
@@ -217,7 +217,7 @@ public class StandardFetcher implements BlojsomFetcher, BlojsomConstants {
                 if ("/".equals(category)) {
                     blogEntry.setLink(blog.getBlogURL() + '?' + PERMALINK_PARAM + '=' + BlojsomUtils.urlEncode(entry.getName()));
                 } else {
-                    blogEntry.setLink(blog.getBlogURL() + BlojsomUtils.urlEncode(category) + "?" + PERMALINK_PARAM + '=' + BlojsomUtils.urlEncode(entry.getName()));
+                    blogEntry.setLink(blog.getBlogURL() + BlojsomUtils.urlEncode(category.substring(0, category.length() - 1)) + "/?" + PERMALINK_PARAM + '=' + BlojsomUtils.urlEncode(entry.getName()));
                 }
                 blogEntry.setBlogFileEncoding(blog.getBlogFileEncoding());
                 blogEntry.setBlogCategory(blogCategoryForEntry);
