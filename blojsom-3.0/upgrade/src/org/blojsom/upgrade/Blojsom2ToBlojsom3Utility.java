1774b6c89288d8ede305499e98f86d13d50756cd
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2006-09-27 16:48:04 +0000

    Progress check-in.

--- blojsom-3.0/upgrade/src/org/blojsom/upgrade/Blojsom2ToBlojsom3Utility.java
+++ blojsom-3.0/upgrade/src/org/blojsom/upgrade/Blojsom2ToBlojsom3Utility.java
@@ -33,20 +33,38 @@ package org.blojsom.upgrade;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.blojsom.fetcher.Fetcher;
+import org.blojsom.util.BlojsomConstants;
+import org.blojsom.util.BlojsomUtils;
+import org.blojsom2.util.BlojsomProperties;
+import org.blojsom2.blog.BlojsomConfiguration;
+import org.blojsom2.blog.BlojsomConfigurationException;
+import org.blojsom2.fetcher.BlojsomFetcher;
+import org.blojsom2.fetcher.BlojsomFetcherException;
+import org.springframework.beans.FatalBeanException;
+import org.springframework.beans.InvalidPropertyException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Utility class to migrate from blojsom 2 to blojsom 3
  *
  * @author David Czarnecki
  * @since blojsom 3
- * @version $Id: Blojsom2ToBlojsom3Utility.java,v 1.1 2006-09-27 01:59:47 czarneckid Exp $
+ * @version $Id: Blojsom2ToBlojsom3Utility.java,v 1.2 2006-09-27 16:48:04 czarneckid Exp $
  */
 public class Blojsom2ToBlojsom3Utility {
 
     private static Log _logger = LogFactory.getLog(Blojsom2ToBlojsom3Utility.class);
 
+    private ServletConfig _servletConfig;
     private Fetcher _fetcher;
+    private BlojsomFetcher _blojsom2Fetcher;
+
     private String _blojsom2Path;
+    private String _blojsom3Path;
 
     /**
      * Construct a new instance of the blojsom 2 to blojsom 3 utility
@@ -64,6 +82,15 @@ public class Blojsom2ToBlojsom3Utility {
     }
 
     /**
+     * Set the path to the blojsom 3 installation directory
+     *
+     * @param blojsom3Path blojsom 3 installation directory
+     */
+    public void setBlojsom3Path(String blojsom3Path) {
+        _blojsom3Path = blojsom3Path;
+    }
+
+    /**
      * Set the {@link Fetcher}
      *
      * @param fetcher {@link Fetcher}
@@ -73,9 +100,101 @@ public class Blojsom2ToBlojsom3Utility {
     }
 
     /**
+     * Set the {@link ServletConfig}
+     *
+     * @param servletConfig {@link ServletConfig}
+     */
+    public void setServletConfig(ServletConfig servletConfig) {
+        _servletConfig = servletConfig;
+    }
+
+    /**
+     * Configure the {@link BlojsomFetcher} that will be used to fetch categories and
+     * entries
+     *
+     * @param servletConfig Servlet configuration information
+     * @param blojsomConfiguration blojsom properties
+     * @throws javax.servlet.ServletException If the {@link BlojsomFetcher} class could not be loaded and/or initialized
+     */
+    protected void configureFetcher(ServletConfig servletConfig, BlojsomConfiguration blojsomConfiguration) throws ServletException {
+        String fetcherClassName = blojsomConfiguration.getFetcherClass();
+        try {
+            Class fetcherClass = Class.forName(fetcherClassName);
+            _blojsom2Fetcher = (BlojsomFetcher) fetcherClass.newInstance();
+            _blojsom2Fetcher.init(servletConfig, blojsomConfiguration);
+            _logger.info("Added blojsom fetcher: " + fetcherClassName);
+        } catch (ClassNotFoundException e) {
+            _logger.error(e);
+            throw new ServletException(e);
+        } catch (InstantiationException e) {
+            _logger.error(e);
+            throw new ServletException(e);
+        } catch (IllegalAccessException e) {
+            _logger.error(e);
+            throw new ServletException(e);
+        } catch (BlojsomFetcherException e) {
+            _logger.error(e);
+            throw new ServletException(e);
+        }
+    }
+
+    /**
      * Upgrade the blojsom 2 instance to blojsom 3
      */
     public void upgrade() {
+        if (BlojsomUtils.checkNullOrBlank(_blojsom2Path)) {
+            throw new InvalidPropertyException(Blojsom2ToBlojsom3Utility.class, "blojsom2Path", "blojsom2Path property was null or blank");
+        }
+
+        if (BlojsomUtils.checkNullOrBlank(_blojsom3Path)) {
+            throw new InvalidPropertyException(Blojsom2ToBlojsom3Utility.class, "blojsom3Path", "blojsom3Path property was null or blank");
+        }
+
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("blojsom 2 path: " + _blojsom2Path);
+            _logger.debug("blojsom 3 path: " + _blojsom3Path);
+        }
+
+        String blojsomPropertiesPath = _blojsom2Path + "/" + BlojsomConstants.DEFAULT_CONFIGURATION_BASE_DIRECTORY + "/blojsom.properties";
+        BlojsomProperties blojsomProperties = null;
+        try {
+            blojsomProperties = new BlojsomProperties();
+            blojsomProperties.load(new FileInputStream(blojsomPropertiesPath));
+        } catch (IOException e) {
+            if (_logger.isErrorEnabled()) {
+                _logger.error(e);
+            }
+
+            blojsomProperties = null;
+        }
+
+        if (blojsomProperties == null) {
+            throw new FatalBeanException("Unable to load blojsom properties file: " + blojsomPropertiesPath);
+        }
+
+        BlojsomConfiguration blojsomConfiguration = null;
+        try {
+            blojsomConfiguration = new BlojsomConfiguration(_servletConfig, BlojsomUtils.propertiesToMap(blojsomProperties));
+        } catch (BlojsomConfigurationException e) {
+            if (_logger.isErrorEnabled()) {
+                _logger.error(e);
+            }
+        }
+
+        if (blojsomConfiguration == null) {
+            throw new FatalBeanException("Unable to construct blojsom configuration object");
+        }
+
+        try {
+            configureFetcher(_servletConfig, blojsomConfiguration);
+        } catch (ServletException e) {
+            if (_logger.isErrorEnabled()) {
+                _logger.error(e);
+            }
+
+            throw new FatalBeanException("Unable to construct blojsom 2 fetcher object", e);
+        }
+
         if (_logger.isDebugEnabled()) {
             _logger.debug("Finished upgrading blojsom 2 instance to blojsom 3!");
         }
