74e59ab348b14d71708e512e3f6ba95d42b38eff
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2006-09-28 05:48:53 +0000

    Progress check-in.

--- blojsom-3.0/upgrade/src/org/blojsom/upgrade/Blojsom2ToBlojsom3Utility.java
+++ blojsom-3.0/upgrade/src/org/blojsom/upgrade/Blojsom2ToBlojsom3Utility.java
@@ -35,13 +35,16 @@ import org.apache.commons.logging.LogFactory;
 import org.blojsom.fetcher.Fetcher;
 import org.blojsom.fetcher.FetcherException;
 import org.blojsom.util.BlojsomConstants;
+import org.blojsom.util.BlojsomMetaDataConstants;
 //import org.blojsom.util.BlojsomUtils;
 import org.blojsom.blog.Blog;
+import org.blojsom.blog.Category;
+import org.blojsom.blog.Entry;
 import org.blojsom.blog.database.DatabaseBlog;
+import org.blojsom.blog.database.DatabaseCategory;
+import org.blojsom.blog.database.DatabaseEntry;
 import org.blojsom2.util.BlojsomProperties;
-import org.blojsom2.blog.BlojsomConfiguration;
-import org.blojsom2.blog.BlojsomConfigurationException;
-import org.blojsom2.blog.BlogUser;
+import org.blojsom2.blog.*;
 import org.blojsom2.fetcher.BlojsomFetcher;
 import org.blojsom2.fetcher.BlojsomFetcherException;
 import org.blojsom2.BlojsomException;
@@ -63,7 +66,7 @@ import java.util.HashMap;
  *
  * @author David Czarnecki
  * @since blojsom 3
- * @version $Id: Blojsom2ToBlojsom3Utility.java,v 1.3 2006-09-27 19:40:42 czarneckid Exp $
+ * @version $Id: Blojsom2ToBlojsom3Utility.java,v 1.4 2006-09-28 05:48:53 czarneckid Exp $
  */
 public class Blojsom2ToBlojsom3Utility {
 
@@ -244,9 +247,37 @@ public class Blojsom2ToBlojsom3Utility {
             // Migrate the properties
             Map blojsom2BlogProperties = blogUser.getBlog().getBlogProperties();
             Properties blogProperties = org.blojsom2.util.BlojsomUtils.mapToProperties(blojsom2BlogProperties);
+            // Remove unused blog properties in blojsom 3
+            blogProperties.remove("blog-home");
+            blogProperties.remove("blog-directory-depth");
+            blogProperties.remove("blog-file-extensions");
+            blogProperties.remove("blog-entry-meta-data-extension");
+            blogProperties.remove("blog-properties-extensions");
+            blogProperties.remove("blog-default-category-mapping");
+            blogProperties.remove("blog-comments-directory");
+            blogProperties.remove("blog-trackbacks-directory");
+            blogProperties.remove("blog-pingbacks-directory");
+            blogProperties.remove("blog-xmlrpc-entry-extension");
+            blogProperties.remove("blog-directory-filter");
+            blogProperties.remove("blog-blacklist-file");
             blog.setProperties(org.blojsom.util.BlojsomUtils.propertiesToMap(blogProperties));
-            blog.setPlugins(blogUser.getPluginChain());
-            // Convert the flavors
+
+            // Migrate the plugin chains
+            Map blojsom2PluginChains = blogUser.getPluginChain();
+            Map blojsom3PluginChains = new HashMap();
+            Iterator pluginIterator = blojsom2PluginChains.keySet().iterator();
+            while (pluginIterator.hasNext()) {
+                String pluginChainForFlavor = (String) pluginIterator.next();
+                String[] flavorForPluginChain = pluginChainForFlavor.split("\\.");
+                if (flavorForPluginChain.length == 2) {
+                    blojsom3PluginChains.put(flavorForPluginChain[0], blojsom2PluginChains.get(pluginChainForFlavor));
+                } else {
+                    blojsom3PluginChains.put("default", blojsom2PluginChains.get(pluginChainForFlavor));
+                }
+            }
+            blog.setPlugins(blojsom3PluginChains);
+
+            // Migrate the flavor and template mappings
             Map blojsom2BlogFlavors = blogUser.getFlavors();
             Map blojsom3FlavorToTemplate = new HashMap();
             Iterator flavorIterator = blojsom2BlogFlavors.keySet().iterator();
@@ -256,11 +287,127 @@ public class Blojsom2ToBlojsom3Utility {
             }
             blog.setTemplates(blojsom3FlavorToTemplate);
 
+            // Save the blojsom 3 blog
             try {
                 _fetcher.saveBlog(blog);
+                blog = _fetcher.loadBlog(blog.getBlogId());
             } catch (FetcherException e) {
                 if (_logger.isErrorEnabled()) {
                     _logger.error(e);
+                }
+
+                continue;
+            }
+
+            // Migrate the users and permissions for each user
+
+            // Migrate the categories
+            Map blojsom2CategoriesMap = new HashMap();
+            Map blojsom3CategoriesMap = new HashMap();
+            try {
+                BlogCategory[] blojsom2Categories = _blojsom2Fetcher.fetchCategories(null, blogUser);
+                for (int j = 0; j < blojsom2Categories.length; j++) {
+                    BlogCategory blojsom2Category = blojsom2Categories[j];
+                    Category blojsom3Category = new DatabaseCategory();
+                    blojsom3Category.setBlogId(blog.getId());
+                    if (org.blojsom.util.BlojsomUtils.checkNullOrBlank(blojsom2Category.getDescription())) {
+                        blojsom3Category.setDescription(blojsom2Category.getEncodedCategory().replaceAll("/", " "));
+                    } else {
+                        blojsom3Category.setDescription(blojsom2Category.getDescription());
+                    }
+                    blojsom3Category.setName(blojsom2Category.getEncodedCategory());
+                    blojsom3Category.setParentCategoryId(null);
+                    blojsom3Category.setMetaData(blojsom2Category.getMetaData());
+
+                    try {
+                        _fetcher.saveCategory(blog, blojsom3Category);
+                        if (_logger.isDebugEnabled()) {
+                            _logger.debug("Created blojsom 3 category: " + blojsom3Category.getName());
+                        }
+
+                        blojsom3Category = _fetcher.loadCategory(blog, blojsom3Category.getName());
+                        blojsom2CategoriesMap.put(blojsom2Category.getEncodedCategory(), blojsom2Category);
+                        blojsom3CategoriesMap.put(blojsom2Category.getEncodedCategory(), blojsom3Category);
+                    } catch (FetcherException e) {
+                        if (_logger.isErrorEnabled()) {
+                            _logger.error(e);
+                        }
+                    }
+                }
+            } catch (BlojsomFetcherException e) {
+                if (_logger.isErrorEnabled()) {
+                    _logger.error(e);
+                }
+
+                continue;
+            }
+
+            // Migrate the entries
+            Iterator blojsom2CategoriesIterator = blojsom2CategoriesMap.keySet().iterator();
+            while (blojsom2CategoriesIterator.hasNext()) {
+                String categoryName = (String) blojsom2CategoriesIterator.next();
+                BlogCategory blogCategory = (BlogCategory) blojsom2CategoriesMap.get(categoryName);
+
+                Map fetchParameters = new HashMap();
+                fetchParameters.put(BlojsomFetcher.FETCHER_CATEGORY, blogCategory);
+                fetchParameters.put(BlojsomFetcher.FETCHER_NUM_POSTS_INTEGER, new Integer(-1));
+                try {
+                    BlogEntry[] entries = _blojsom2Fetcher.fetchEntries(fetchParameters, blogUser);
+
+                    if (_logger.isDebugEnabled()) {
+                        _logger.debug("Migrating " + entries.length + " entries from blojsom 2 category: " + blogCategory.getEncodedCategory());
+                    }
+
+                    for (int j = 0; j < entries.length; j++) {
+                        BlogEntry entry = entries[j];
+                        Entry blojsom3Entry = new DatabaseEntry();
+                        blojsom3Entry.setBlogId(blog.getId());
+
+                        Category category = (Category) blojsom3CategoriesMap.get(blogCategory.getEncodedCategory());
+                        blojsom3Entry.setBlogCategoryId(category.getId());
+                        if (entry.getMetaData().get("blog-entry-author") != null) {
+                            blojsom3Entry.setAuthor(entry.getMetaData().get("blog-entry-author").toString());
+                        }
+                        //blojsom3Entry.setComments(null);
+                        if (entry.supportsComments()) {
+                            blojsom3Entry.setAllowComments(new Integer(1));
+                        } else {
+                            blojsom3Entry.setAllowComments(new Integer(0));
+                        }
+                        blojsom3Entry.setDate(entry.getDate());
+                        blojsom3Entry.setDescription(entry.getDescription());
+                        blojsom3Entry.setMetaData(entry.getMetaData());
+                        blojsom3Entry.setModifiedDate(entry.getDate());
+                        if (entry.supportsPingbacks()) {
+                            blojsom3Entry.setAllowPingbacks(new Integer(1));
+                        } else {
+                            blojsom3Entry.setAllowPingbacks(new Integer(0));
+                        }
+                        //blojsom3Entry.setPingbacks(null);
+                        blojsom3Entry.setPostSlug(entry.getPermalink());
+                        blojsom3Entry.setStatus(BlojsomMetaDataConstants.PUBLISHED_STATUS);
+                        blojsom3Entry.setTitle(entry.getTitle());
+                        if (entry.supportsTrackbacks()) {
+                            blojsom3Entry.setAllowTrackbacks(new Integer(1));
+                        } else {
+                            blojsom3Entry.setAllowTrackbacks(new Integer(0));
+                        }
+                        //blojsom3Entry.setTrackbacks(null);
+
+                        try {
+                            _fetcher.saveEntry(blog, blojsom3Entry);
+                        } catch (FetcherException e) {
+                            if (_logger.isErrorEnabled()) {
+                                _logger.error(e);
+                            }
+                        }
+                    }
+                } catch (BlojsomFetcherException e) {
+                    if (_logger.isErrorEnabled()) {
+                        _logger.error(e);
+                    }
+
+                    continue;
                 }
             }
         }
