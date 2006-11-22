7030974028ac88b59c4d2e3aa97f9c72135e1d56
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2006-11-22 01:53:11 +0000

    Added new primary keys for metadata tables and various properties tables.

--- blojsom-3.0/sql-scripts/blojsom-full-initial-data-mysql.sql
+++ blojsom-3.0/sql-scripts/blojsom-full-initial-data-mysql.sql
@@ -78,6 +78,8 @@ CREATE TABLE `CategoryMetadata` (
   `category_id` int(11) NOT NULL,
   `metadata_key` text NOT NULL,
   `metadata_value` text,
+  `category_metadata_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`category_metadata_id`),
   KEY `categorymetadata_category_categoryidfk` (`category_id`),
   CONSTRAINT `categorymetadata_category_categoryidfk` FOREIGN KEY (`category_id`) REFERENCES `Category` (`category_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -133,6 +135,8 @@ CREATE TABLE `CommentMetadata` (
   `comment_id` int(11) NOT NULL,
   `metadata_key` text NOT NULL,
   `metadata_value` text,
+  `comment_metadata_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`comment_metadata_id`),
   KEY `commentmetadata_comment_commentidfk` (`comment_id`),
   CONSTRAINT `commentmetadata_comment_commentidfk` FOREIGN KEY (`comment_id`) REFERENCES `Comment` (`comment_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -248,6 +252,8 @@ CREATE TABLE `EntryMetadata` (
   `entry_id` int(11) NOT NULL,
   `metadata_key` text NOT NULL,
   `metadata_value` text,
+  `entry_metadata_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`entry_metadata_id`),
   KEY `entrymetadata_entry_entryidfk` (`entry_id`),
   CONSTRAINT `entrymetadata_entry_entryidfk` FOREIGN KEY (`entry_id`) REFERENCES `Entry` (`entry_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -304,6 +310,8 @@ CREATE TABLE `PingbackMetadata` (
   `pingback_id` int(11) NOT NULL,
   `metadata_key` text NOT NULL,
   `metadata_value` text,
+  `pingback_metadata_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`pingback_metadata_id`),
   KEY `pingbackmetadata_pingback_pingbackidfk` (`pingback_id`),
   CONSTRAINT `pingbackmetadata_pingback_pingbackidfk` FOREIGN KEY (`pingback_id`) REFERENCES `Pingback` (`pingback_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -327,6 +335,8 @@ CREATE TABLE `Plugin` (
   `blog_id` int(11) NOT NULL,
   `plugin_flavor` varchar(50) NOT NULL,
   `plugin_value` varchar(4096) default NULL,
+  `plugin_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`plugin_id`),
   KEY `plugin_blog_blogidfk` (`blog_id`),
   CONSTRAINT `plugin_blog_blogidfk` FOREIGN KEY (`blog_id`) REFERENCES `Blog` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -338,7 +348,7 @@ CREATE TABLE `Plugin` (
 
 /*!40000 ALTER TABLE `Plugin` DISABLE KEYS */;
 LOCK TABLES `Plugin` WRITE;
-INSERT INTO `Plugin` VALUES (1,'html','meta, tag-cloud, date-format, referer-log, calendar-gui, calendar-filter, comment, trackback, simple-search, emoticons, macro-expansion, days-since-posted, word-count, simple-obfuscation, nofollow, rss-enclosure, technorati-tags'),(1,'default','conditional-get, meta, nofollow, rss-enclosure'),(1,'admin','admin');
+INSERT INTO `Plugin` VALUES (1,'html','meta, tag-cloud, date-format, referer-log, calendar-gui, calendar-filter, comment, trackback, simple-search, emoticons, macro-expansion, days-since-posted, word-count, simple-obfuscation, nofollow, rss-enclosure, technorati-tags',1),(1,'default','conditional-get, meta, nofollow, rss-enclosure',2),(1,'admin','admin',3);
 UNLOCK TABLES;
 /*!40000 ALTER TABLE `Plugin` ENABLE KEYS */;
 
@@ -351,6 +361,8 @@ CREATE TABLE `Properties` (
   `blog_id` int(11) NOT NULL,
   `property_name` varchar(255) NOT NULL,
   `property_value` longtext,
+  `property_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`property_id`),
   KEY `properties_blog_blogidfk` (`blog_id`),
   CONSTRAINT `properties_blog_blogidfk` FOREIGN KEY (`blog_id`) REFERENCES `Blog` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -362,7 +374,7 @@ CREATE TABLE `Properties` (
 
 /*!40000 ALTER TABLE `Properties` DISABLE KEYS */;
 LOCK TABLES `Properties` WRITE;
-INSERT INTO `Properties` VALUES (1,'blog-url','http://localhost:8080/blojsom/blog/default'),(1,'blog-admin-url','http://localhost:8080/blojsom/blog/default'),(1,'blog-base-url','http://localhost:8080/blojsom'),(1,'blog-base-admin-url','http://localhost:8080/blojsom'),(1,'blog-language','en'),(1,'blog-name','NAME YOUR BLOG'),(1,'blog-description','DESCRIBE YOUR BLOG'),(1,'blog-entries-display','15'),(1,'blog-owner','Default Owner'),(1,'blog-owner-email','default_owner@email.com'),(1,'blog-comments-enabled','true'),(1,'blog-trackbacks-enabled','true'),(1,'blog-email-enabled','true'),(1,'blog-default-flavor','html'),(1,'plugin-comment-autoformat','true'),(1,'linear-navigation-enabled','true'),(1,'comment-moderation-enabled','true'),(1,'trackback-moderation-enabled','true'),(1,'pingback-moderation-enabled','true'),(1,'blog-ping-urls','http://rpc.pingomatic.com'),(1,'blojsom-extension-metaweblog-accepted-types','image/jpeg, image/gif, image/png, img'),(1,'xmlrpc-enabled','true'),(1,'blog-pingbacks-enabled','true');
+INSERT INTO `Properties` VALUES (1,'blog-url','http://localhost:8080/blojsom/blog/default',1),(1,'blog-admin-url','http://localhost:8080/blojsom/blog/default',2),(1,'blog-base-url','http://localhost:8080/blojsom',3),(1,'blog-base-admin-url','http://localhost:8080/blojsom',4),(1,'blog-language','en',5),(1,'blog-name','NAME YOUR BLOG',6),(1,'blog-description','DESCRIBE YOUR BLOG',7),(1,'blog-entries-display','15',8),(1,'blog-owner','Default Owner',9),(1,'blog-owner-email','default_owner@email.com',10),(1,'blog-comments-enabled','true',11),(1,'blog-trackbacks-enabled','true',12),(1,'blog-email-enabled','true',13),(1,'blog-default-flavor','html',14),(1,'plugin-comment-autoformat','true',15),(1,'linear-navigation-enabled','true',16),(1,'comment-moderation-enabled','true',17),(1,'trackback-moderation-enabled','true',18),(1,'pingback-moderation-enabled','true',19),(1,'blog-ping-urls','http://rpc.pingomatic.com',20),(1,'blojsom-extension-metaweblog-accepted-types','image/jpeg, image/gif, image/png, img',21),(1,'xmlrpc-enabled','true',22),(1,'blog-pingbacks-enabled','true',23);
 UNLOCK TABLES;
 /*!40000 ALTER TABLE `Properties` ENABLE KEYS */;
 
@@ -375,6 +387,8 @@ CREATE TABLE `Template` (
   `blog_id` int(11) NOT NULL,
   `template_flavor` varchar(50) NOT NULL,
   `template_value` varchar(255) default NULL,
+  `template_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`template_id`),
   KEY `template_blog_blogidfk` (`blog_id`),
   CONSTRAINT `template_blog_blogidfk` FOREIGN KEY (`blog_id`) REFERENCES `Blog` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -386,7 +400,7 @@ CREATE TABLE `Template` (
 
 /*!40000 ALTER TABLE `Template` DISABLE KEYS */;
 LOCK TABLES `Template` WRITE;
-INSERT INTO `Template` VALUES (1,'rss','rss.vm, text/xml;charset=UTF-8'),(1,'rsd','rsd.vm, application/rsd+xml;charset=UTF-8'),(1,'html','asual.vm, text/html;charset=UTF-8'),(1,'atom','atom.vm, application/atom+xml;charset=UTF-8'),(1,'rss2','rss2.vm, text/xml;charset=UTF-8'),(1,'rdf','rdf.vm, text/xml;charset=UTF-8'),(1,'admin','org/blojsom/plugin/admin/templates/admin.vm, text/html;charset=UTF-8');
+INSERT INTO `Template` VALUES (1,'rss','rss.vm, text/xml;charset=UTF-8',1),(1,'rsd','rsd.vm, application/rsd+xml;charset=UTF-8',2),(1,'html','asual.vm, text/html;charset=UTF-8',3),(1,'atom','atom.vm, application/atom+xml;charset=UTF-8',4),(1,'rss2','rss2.vm, text/xml;charset=UTF-8',5),(1,'rdf','rdf.vm, text/xml;charset=UTF-8',6),(1,'admin','org/blojsom/plugin/admin/templates/admin.vm, text/html;charset=UTF-8',7);
 UNLOCK TABLES;
 /*!40000 ALTER TABLE `Template` ENABLE KEYS */;
 
@@ -430,6 +444,8 @@ CREATE TABLE `TrackbackMetadata` (
   `trackback_id` int(11) NOT NULL,
   `metadata_key` text NOT NULL,
   `metadata_value` text,
+  `trackback_metadata_id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY(`trackback_metadata_id`),
   KEY `trackbackmetadata_trackback_trackbackidfk` (`trackback_id`),
   CONSTRAINT `trackbackmetadata_trackback_trackbackidfk` FOREIGN KEY (`trackback_id`) REFERENCES `Trackback` (`trackback_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
