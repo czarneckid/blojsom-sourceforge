2a8aea1ea90b3dd58baf339396684fe578ae0650
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2006-12-15 19:21:49 +0000

    Moved Skip Filter Helper plugin to skip package.

--- blojsom-3.0/plugins/src/org/blojsom/plugin/skip/SkipFilterHelperPlugin.java
+++ blojsom-3.0/plugins/src/org/blojsom/plugin/skip/SkipFilterHelperPlugin.java
@@ -28,7 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.blojsom.plugin.statistics;
+package org.blojsom.plugin.skip;
 
 import org.blojsom.plugin.Plugin;
 import org.blojsom.plugin.PluginException;
@@ -50,7 +50,7 @@ import java.util.Map;
  *
  * @author David Czarnecki
  * @since blojsom 3.1
- * @version $Id: SkipFilterHelperPlugin.java,v 1.1 2006-10-06 20:08:24 czarneckid Exp $
+ * @version $Id: SkipFilterHelperPlugin.java,v 1.1 2006-12-15 19:21:02 czarneckid Exp $
  */
 public class SkipFilterHelperPlugin implements Plugin {
 
