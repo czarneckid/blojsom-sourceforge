14ae2854074f20875776db369575f7bc1bf1a618
czarneckid <czarneckid@28e03f91-8513-0410-9821-a6140ff45be5>
2007-05-17 19:11:43 +0000

    Fix for BLOJSOM-223 ( Implement plugin to allow for lookup of user name from template )
    http://jira.blojsom.com/jira/browse/BLOJSOM-223

---
+++ blojsom-3.0/plugins/src/org/blojsom/plugin/user/UserLookupPlugin.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (c) 2003-2007, David A. Czarnecki
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *     following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *     following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of "David A. Czarnecki" and "blojsom" nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior written permission.
+ * Products derived from this software may not be called "blojsom", nor may "blojsom" appear in their name,
+ *     without prior written permission of David A. Czarnecki.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.blojsom.plugin.user;
+
+import org.blojsom.blog.Blog;
+import org.blojsom.blog.Entry;
+import org.blojsom.fetcher.Fetcher;
+import org.blojsom.fetcher.FetcherException;
+import org.blojsom.plugin.Plugin;
+import org.blojsom.plugin.PluginException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * UserLookupPlugin
+ *
+ * @author David Czarnecki
+ * @since blojsom 3.3
+ * @version $Id: UserLookupPlugin.java,v 1.1 2007-05-17 19:11:43 czarneckid Exp $
+ */
+public class UserLookupPlugin implements Plugin {
+
+    private Fetcher _fetcher;
+
+    private static final String USER_LOOKUP_PLUGIN_LOOKUP_OBJECT = "USER_LOOKUP_PLUGIN_LOOKUP_OBJECT";
+    
+    /**
+     * Construct a new instance of the user lookup plugin
+     */
+    public UserLookupPlugin() {
+    }
+
+    /**
+     * Set the {@link Fetcher}
+     *
+     * @param fetcher {@link Fetcher}
+     */
+    public void setFetcher(Fetcher fetcher) {
+        _fetcher = fetcher;
+    }
+
+    /**
+     * Initialize this plugin. This method only called when the plugin is instantiated.
+     *
+     * @throws PluginException If there is an error initializing the plugin
+     */
+    public void init() throws PluginException {
+    }
+
+    /**
+     * Process the blog entries
+     *
+     * @param httpServletRequest  Request
+     * @param httpServletResponse Response
+     * @param blog                {@link Blog} instance
+     * @param context             Context
+     * @param entries             Blog entries retrieved for the particular request
+     * @return Modified set of blog entries
+     * @throws PluginException If there is an error processing the blog entries
+     */
+    public Entry[] process(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Blog blog, Map context, Entry[] entries) throws PluginException {
+        context.put(USER_LOOKUP_PLUGIN_LOOKUP_OBJECT, new UserLookup(_fetcher));
+        
+        return entries;
+    }
+
+    /**
+     * Perform any cleanup for the plugin. Called after {@link #process}.
+     *
+     * @throws PluginException If there is an error performing cleanup for this plugin
+     */
+    public void cleanup() throws PluginException {
+    }
+
+    /**
+     * Called when BlojsomServlet is taken out of service
+     *
+     * @throws PluginException If there is an error in finalizing this plugin
+     */
+    public void destroy() throws PluginException {
+    }
+
+    /**
+     * User lookup 
+     */
+    public class UserLookup {
+
+        private Fetcher _fetcher;
+
+        /**
+         * Construct a new user lookup object
+         *
+         * @param _fetcher {@link Fetcher}
+         */
+        public UserLookup(Fetcher _fetcher) {
+            this._fetcher = _fetcher;
+        }
+
+        /**
+         * Lookup a user name for a given loginID
+         *
+         * @param blog {@link Blog}
+         * @param loginID Login ID
+         * @return User name or <code>null</code> if there was an error looking up the user name
+         */
+        public String getUserName(Blog blog, String loginID) {
+            try {
+                return _fetcher.loadUser(blog, loginID).getUserName();
+            } catch (FetcherException e) {
+                return null;
+            }
+        }
+    }
+}
